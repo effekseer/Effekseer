e8cb34007eff3f48e5a24abfbdf8b148b3824d58
ueshita <nalto32@gmail.com>
2015-10-31 13:23:38 +0900

    [Unity] EffekseerPluginCommon.hを追加

---
+++ Dev/Cpp/UnityPlugin/common/EffekseerPluginCommon.h
@@ -0,0 +1,18 @@
+
+#ifndef	__EFFEKSEER_PLUGIN_COMMON_H__
+#define __EFFEKSEER_PLUGIN_COMMON_H__
+
+#include <vector>
+#include "Effekseer.h"
+#include "IUnityInterface.h"
+
+#define DLLEXPORT UNITY_INTERFACE_EXPORT
+#define UNITY_API UNITY_INTERFACE_API
+
+extern Effekseer::Manager*				g_EffekseerManager;
+
+const int MAX_RENDER_PATH = 128;
+extern Effekseer::Matrix44	g_cameraMatrix[MAX_RENDER_PATH];
+extern Effekseer::Matrix44	g_projectionMatrix[MAX_RENDER_PATH];
+
+#endif
