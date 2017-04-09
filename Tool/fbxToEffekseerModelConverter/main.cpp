0848cecc53ed741f60ca4cdf3a399901631fabac
effekseer <effekseer@gmail.com>
2017-04-10 01:45:08 +0900

    Parse arges.

--- Tool/fbxToEffekseerModelConverter/main.cpp
+++ Tool/fbxToEffekseerModelConverter/main.cpp
@@ -13,12 +13,76 @@ int main(int argc, char** argv)
 {
 	if(argc == 1)
 	{
-		printf("Lack arges");
+		printf("Effekseer Model Conveter\n");
+		printf("Usage: mqoToEffekseerModelConverter InputFile(*.mqo) {OutputFile(*.efkmodel)} {options}\n");
+		printf("");
+		printf("Options: -modelcount : Max Render Count (1 - n)\n");
+		printf("         -scale      : Scaling (ex. 0.5, 1.2)\n");
+		printf("");
+		printf("Examples: fbxToEffekseerModelConverter foo.mqo -scale 0.1\n");
 		return -1;
 	}
 
-	const char* importPath = argv[1];
-	const char* exportPath = argv[2];
+	std::map<std::string, std::string> args;
+
+	for (int32_t i = 1; i < argc - 1; i++)
+	{
+		if (argv[i][0] == '-')
+		{
+			args[argv[i]] = argv[i + 1];
+		}
+	}
+
+	std::string importPath = "";
+	std::string exportPath = "";
+
+	if (argc >= 2)
+	{
+		std::string p = argv[1];
+		int ext_i = p.find_last_of(".");
+		std::string ext = p.substr(ext_i, p.size() - ext_i);
+
+		if (ext != ".fbx")
+		{
+			printf("InputFile is not fbx\n");
+			return -1;
+		}
+		importPath = argv[1];
+	}
+
+	if (argc >= 3)
+	{
+		std::string p = argv[2];
+		int ext_i = p.find_last_of(".");
+		std::string ext = p.substr(ext_i, p.size() - ext_i);
+
+		if (ext != ".efkmodel")
+		{
+			printf("OutputFile is not efkmodel\n");
+			return -1;
+		}
+		exportPath = argv[2];
+	}
+	else
+	{
+		std::string p = argv[1];
+		int ext_i = p.find_last_of(".");
+		std::string p_ = p.substr(0, ext_i);
+		exportPath = p_ + ".efkmodel";
+	}
+
+	int modelCount = 1;
+	float modelScale = 1.0f;
+
+	if (args.count("-modelcount") > 0)
+	{
+		modelCount = ::std::atoi(args["-modelcount"].c_str());
+	}
+
+	if (args.count("-scale") > 0)
+	{
+		modelScale = ::std::atof(args["-scale"].c_str());
+	}
 
 	fbxToEfkMdl::FBXConverter converter;
 
@@ -27,7 +91,7 @@ int main(int argc, char** argv)
 	sdkManager->SetIOSettings(ios);
 
 	fbxsdk::FbxImporter* fbxImporter = fbxsdk::FbxImporter::Create(sdkManager, "");
-	if (!fbxImporter->Initialize(importPath, -1, sdkManager->GetIOSettings()))
+	if (!fbxImporter->Initialize(importPath.c_str(), -1, sdkManager->GetIOSettings()))
 	{
 		printf("Call to FbxImporter::Initialize() failed.\n");
 		printf("Error returned: %s\n\n", fbxImporter->GetStatus().GetErrorString());
