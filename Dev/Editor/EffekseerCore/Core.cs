012d441df116dcdf0d6d816c74e1b8e710a6a7b3
effekseer <effekseer@gmail.com>
2017-09-27 01:15:06 +0900

    RC

--- Dev/Editor/EffekseerCore/Core.cs
+++ Dev/Editor/EffekseerCore/Core.cs
@@ -10,7 +10,7 @@ namespace Effekseer
 {
 	public class Core
 	{
-		public const string Version = "1.30β2";
+		public const string Version = "1.30RC";
 
 		public const string OptionFilePath = "config.option.xml";
 
