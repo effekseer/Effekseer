16a9e093a585c2c697560bd412a0591dae178df7
effekseer <effekseer@gmail.com>
2018-06-09 12:53:26 +0900

    Fixed image bug

--- Dev/Editor/EffekseerMP/GUI/Component/PathForImage.cs
+++ Dev/Editor/EffekseerMP/GUI/Component/PathForImage.cs
@@ -34,12 +34,27 @@ namespace Effekseer.GUI.Component
 			}
 			set
 			{
-				if (binding == value) return;
+				if (binding == value)
+				{
+					return;
+				}
+
+				if(value == null)
+				{
+					binding.OnChanged -= Binding_OnChanged;
+					binding = null;
+					return;
+				}
 
 				binding = value;
-
+				binding.OnChanged += Binding_OnChanged;
 				Read();
 			}
+		}
+
+		private void Binding_OnChanged(object sender, ChangedValueEventArgs e)
+		{
+			Read();
 		}
 
 		public PathForImage(string label = null)
@@ -200,24 +215,27 @@ namespace Effekseer.GUI.Component
 			if (binding != null)
 			{
 				filePath = binding.GetRelativePath();
-				if (filePath.Length > 0)
-				{
-					UpdateInfo();
-				}
-				else
-				{
-				}
+				UpdateInfo();
 			}
 			else
 			{
 				filePath = string.Empty;
+				UpdateInfo();
 			}
 		}
 
 		void UpdateInfo()
 		{
 			string path = binding.GetAbsolutePath();
-			image = Images.Load(Manager.Native, path);
+
+			if(System.IO.File.Exists(path))
+			{
+				image = Images.Load(Manager.Native, path);
+			}
+			else
+			{
+				image = null;
+			}
 		}
 
 		private bool CheckExtension(string path)
