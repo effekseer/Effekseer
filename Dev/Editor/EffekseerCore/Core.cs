f86b1a959e6b5edd7054727069b6d6077e8bd578
effekseer <effekseer@gmail.com>
2018-08-29 00:35:12 +0900

    1.40beta3

--- Dev/Editor/EffekseerCore/Core.cs
+++ Dev/Editor/EffekseerCore/Core.cs
@@ -10,7 +10,7 @@ namespace Effekseer
 {
 	public class Core
 	{
-		public const string Version = "1.40β2";
+		public const string Version = "1.40β3";
 
 		public const string OptionFilePath = "config.option.xml";
 
