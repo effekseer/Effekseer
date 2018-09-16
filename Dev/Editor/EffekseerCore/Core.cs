c7e1227c6a7f897465a4993a94597bfaaee1ce54
effekseer <effekseer@gmail.com>
2018-09-16 23:32:54 +0900

    142a

--- Dev/Editor/EffekseerCore/Core.cs
+++ Dev/Editor/EffekseerCore/Core.cs
@@ -10,7 +10,7 @@ namespace Effekseer
 {
 	public class Core
 	{
-		public const string Version = "1.42";
+		public const string Version = "1.42a";
 
 		public const string OptionFilePath = "config.option.xml";
 
