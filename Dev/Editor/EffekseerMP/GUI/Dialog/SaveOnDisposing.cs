da084d8c53f42e0c8fbeadb08966daf1519a7ae9
effekseer <effekseer@gmail.com>
2018-10-14 20:09:38 +0900

    Fixed a bug shows multiple window when clicking a close button

--- Dev/Editor/EffekseerMP/GUI/Dialog/SaveOnDisposing.cs
+++ Dev/Editor/EffekseerMP/GUI/Dialog/SaveOnDisposing.cs
@@ -1,4 +1,8 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace Effekseer.GUI.Dialog
 {
@@ -23,6 +27,13 @@ namespace Effekseer.GUI.Dialog
 			message = string.Format(format, System.IO.Path.GetFileName(Core.FullPath));
 
 			this.disposed = disposed;
+
+			// if already show window, don't add control
+			if(Manager.Controls.Internal.Any(_=>_ is SaveOnDisposing))
+			{
+				return;
+			}
+
 			Manager.AddControl(this);
 		}
 
