b8c0d5747cc1c6c7a386087dd3f12d6ab1a60d4b
ueshita <nalto32@gmail.com>
2019-02-11 12:42:57 +0900

    Add tonemap build command.

--- Dev/Cpp/Viewer/Graphics/Platform/DX11/compile_effect.bat
+++ Dev/Cpp/Viewer/Graphics/Platform/DX11/compile_effect.bat
@@ -4,5 +4,6 @@
 "%DXSDK_DIR%Utilities\\bin\x86\fxc" /Zpc /Tps_4_1 /EPS /Fh Shader/efk.GraphicsDX11.PostFX_Blend_PS.h Shader/postfx_blend_PS.fx
 "%DXSDK_DIR%Utilities\\bin\x86\fxc" /Zpc /Tps_4_1 /EPS /Fh Shader/efk.GraphicsDX11.PostFX_BlurH_PS.h Shader/postfx_blur_h_PS.fx
 "%DXSDK_DIR%Utilities\\bin\x86\fxc" /Zpc /Tps_4_1 /EPS /Fh Shader/efk.GraphicsDX11.PostFX_BlurV_PS.h Shader/postfx_blur_v_PS.fx
+"%DXSDK_DIR%Utilities\\bin\x86\fxc" /Zpc /Tps_4_1 /EPS /Fh Shader/efk.GraphicsDX11.PostFX_Tonemap_PS.h Shader/postfx_tonemap_PS.fx
 
 pause
