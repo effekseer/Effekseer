d2ba36ab3a9c762a8b2c5ad890719d3925628261
durswd <swda.durl@gmail.com>
2019-09-07 16:38:30 +0900

    fixed a bug that normal cannot be loaded from fbx generated maya

--- Tool/fbxToEffekseerModelConverter/fbxToEfkMdl.FBXConverter.h
+++ Tool/fbxToEffekseerModelConverter/fbxToEfkMdl.FBXConverter.h
@@ -21,7 +21,7 @@ namespace fbxToEfkMdl
 		void CalcTangentSpace(const Vertex& v1, const Vertex& v2, const Vertex& v3, FbxVector4& binormal, FbxVector4& tangent);
 
 		FbxVector4 LoadPosition(FbxMesh* fbxMesh, int32_t ctrlPointIndex);
-		FbxVector4 LoadNormal(FbxLayerElementNormal* normals, int32_t vertexID, int32_t ctrlPointIndex);
+		FbxVector4 LoadNormal(FbxMesh* fbxMesh, FbxLayerElementNormal* normals, int32_t vertexID, int32_t ctrlPointIndex);
 		FbxVector2 LoadUV(FbxMesh* fbxMesh, FbxLayerElementUV* uvs, int32_t vertexID, int32_t ctrlPointIndex, int32_t polygonIndex, int32_t polygonPointIndex);
 		FbxColor LoadVertexColor(FbxMesh* fbxMesh, FbxLayerElementVertexColor* colors, int32_t vertexID, int32_t ctrlPointIndex, int32_t polygonIndex, int32_t polygonPointIndex);
 
