71e9d393c53cda15829420cfbe5f207321bb4eac
durswd <swda.durl@gmail.com>
2019-09-07 23:39:06 +0900

    implementing for DirectX12

--- Dev/Cpp/EffekseerRendererDX12/EffekseerRenderer/EffekseerRendererDX12.Renderer.cpp
+++ Dev/Cpp/EffekseerRendererDX12/EffekseerRenderer/EffekseerRendererDX12.Renderer.cpp
@@ -2,6 +2,59 @@
 #include "../../3rdParty/LLGI/src/DX12/LLGI.GraphicsDX12.h"
 #include "../EffekseerRendererLLGI/EffekseerRendererLLGI.RendererImplemented.h"
 
+namespace Standard_VS
+{
+static
+#include "Shader/EffekseerRenderer.Standard_VS.h"
+} // namespace Standard_VS
+
+namespace Standard_PS
+{
+static
+#include "Shader/EffekseerRenderer.Standard_PS.h"
+} // namespace Standard_PS
+
+namespace Standard_Distortion_VS
+{
+static
+#include "Shader/EffekseerRenderer.Standard_Distortion_VS.h"
+} // namespace Standard_Distortion_VS
+
+namespace Standard_Distortion_PS
+{
+static
+#include "Shader/EffekseerRenderer.Standard_Distortion_PS.h"
+} // namespace Standard_Distortion_PS
+
+namespace ShaderLightingTextureNormal_
+{
+static
+#include "Shader/EffekseerRenderer.ModelRenderer.ShaderLightingTextureNormal_VS.h"
+
+	static
+#include "Shader/EffekseerRenderer.ModelRenderer.ShaderLightingTextureNormal_PS.h"
+
+} // namespace ShaderLightingTextureNormal_
+
+namespace ShaderTexture_
+{
+static
+#include "Shader/EffekseerRenderer.ModelRenderer.ShaderTexture_VS.h"
+
+	static
+#include "Shader/EffekseerRenderer.ModelRenderer.ShaderTexture_PS.h"
+} // namespace ShaderTexture_
+
+namespace ShaderDistortionTexture_
+{
+static
+#include "Shader/EffekseerRenderer.ModelRenderer.ShaderDistortion_VS.h"
+
+	static
+#include "Shader/EffekseerRenderer.ModelRenderer.ShaderDistortion_PS.h"
+} // namespace ShaderDistortionTexture_
+
+
 namespace EffekseerRendererDX12
 {
 
@@ -30,45 +83,28 @@ namespace EffekseerRendererDX12
 	::EffekseerRendererLLGI::RendererImplemented* renderer = new ::EffekseerRendererLLGI::RendererImplemented(squareMaxCount);
 
 	auto allocate_ = [](std::vector<LLGI::DataStructure>& ds, const unsigned char* data, int32_t size) -> void {
-
+		ds.resize(1);
+		ds[0].Size = size;
+		ds[0].Data = data;
+		return;
 	};
 
-	/*
-	allocate_(renderer->fixedShader.StandardTexture_VS, g_standard_vsData, g_standard_vsLength);
-	allocate_(renderer->fixedShader.Standard_VS, g_standard_vsData, g_standard_vsLength);
-	allocate_(renderer->fixedShader.StandardTexture_PS, g_standard_psData, g_standard_psLength);
-	allocate_(renderer->fixedShader.Standard_PS, g_standard_no_texture_psData, g_standard_no_texture_psLength);
+	allocate_(renderer->fixedShader_.StandardTexture_VS, Standard_VS::g_VS, sizeof(Standard_VS::g_VS));
+	allocate_(renderer->fixedShader_.StandardTexture_PS, Standard_PS::g_PS, sizeof(Standard_PS::g_PS));
 
-	allocate_(renderer->fixedShader.StandardDistortedTexture_VS, g_standard_vsData, g_standard_vsLength);
-	allocate_(renderer->fixedShader.StandardDistorted_VS, g_standard_vsData, g_standard_vsLength);
-	allocate_(renderer->fixedShader.StandardDistortedTexture_PS, g_standard_psData, g_standard_psLength);
-	allocate_(renderer->fixedShader.StandardDistorted_PS, g_standard_no_texture_psData, g_standard_no_texture_psLength);
+	allocate_(renderer->fixedShader_.StandardDistortedTexture_VS, Standard_Distortion_VS::g_VS, sizeof(Standard_Distortion_VS::g_VS));
+	allocate_(renderer->fixedShader_.StandardDistortedTexture_PS, Standard_Distortion_PS::g_PS, sizeof(Standard_Distortion_PS::g_PS));
 
-	allocate_(renderer->fixedShader.ModelShader_VS, g_model_vsData, g_model_vsLength);
-	allocate_(renderer->fixedShader.ModelShader_PS, g_model_psData, g_model_psLength);
+	allocate_(renderer->fixedShader_.ModelShaderTexture_VS, ShaderTexture_::g_VS, sizeof(ShaderTexture_::g_VS));
+	allocate_(renderer->fixedShader_.ModelShaderTexture_PS, ShaderTexture_::g_PS, sizeof(ShaderTexture_::g_PS));
 
-	allocate_(renderer->fixedShader.ModelShaderLighting_VS, g_model_l_vsData, g_model_l_vsLength);
-	allocate_(renderer->fixedShader.ModelShaderLighting_PS, g_model_l_psData, g_model_l_psLength);
+	allocate_(renderer->fixedShader_.ModelShaderLightingTextureNormal_VS, ShaderLightingTextureNormal_::g_VS, sizeof(ShaderLightingTextureNormal_::g_VS));
+	allocate_(renderer->fixedShader_.ModelShaderLightingTextureNormal_PS, ShaderLightingTextureNormal_::g_PS, sizeof(ShaderLightingTextureNormal_::g_PS));
 
-	allocate_(renderer->fixedShader.ModelShaderTexture_VS, g_model_t_vsData, g_model_t_vsLength);
-	allocate_(renderer->fixedShader.ModelShaderTexture_PS, g_model_t_psData, g_model_t_psLength);
+	allocate_(renderer->fixedShader_.ModelShaderDistortionTexture_VS, ShaderDistortionTexture_::g_VS, sizeof(ShaderDistortionTexture_::g_VS));
+	allocate_(renderer->fixedShader_.ModelShaderDistortionTexture_PS, ShaderDistortionTexture_::g_PS, sizeof(ShaderDistortionTexture_::g_PS));
 
-	allocate_(renderer->fixedShader.ModelShaderLightingTexture_VS, g_model_lt_vsData, g_model_lt_vsLength);
-	allocate_(renderer->fixedShader.ModelShaderLightingTexture_PS, g_model_lt_psData, g_model_lt_psLength);
-
-	allocate_(renderer->fixedShader.ModelShaderLightingNormal_VS, g_model_ln_vsData, g_model_ln_vsLength);
-	allocate_(renderer->fixedShader.ModelShaderLightingNormal_PS, g_model_ln_psData, g_model_ln_psLength);
-
-	allocate_(renderer->fixedShader.ModelShaderLightingTextureNormal_VS, g_model_ltn_vsData, g_model_ltn_vsLength);
-	allocate_(renderer->fixedShader.ModelShaderLightingTextureNormal_PS, g_model_ltn_psData, g_model_ltn_psLength);
-
-	allocate_(renderer->fixedShader.ModelShaderDistortionTexture_VS, g_model_distortion_vsData, g_model_distortion_vsLength);
-	allocate_(renderer->fixedShader.ModelShaderDistortion_VS, g_model_distortion_vsData, g_model_distortion_vsLength);
-	allocate_(renderer->fixedShader.ModelShaderDistortionTexture_PS, g_model_distortion_psData, g_model_distortion_psLength);
-	allocate_(renderer->fixedShader.ModelShaderDistortion_PS, g_model_distortion_no_texture_psData, g_model_distortion_no_texture_psLength);
-	*/
-
-	if (renderer->Initialize(graphics, (renderer->fixedShader_), isReversedDepth))
+	if (renderer->Initialize(graphics, isReversedDepth))
 	{
 		ES_SAFE_RELEASE(graphics);
 		return renderer;
@@ -104,7 +140,7 @@ void DeleteTextureData(::EffekseerRenderer::Renderer* renderer, Effekseer::Textu
 void FlushAndWait(::EffekseerRenderer::Renderer* renderer)
 {
 	auto r = static_cast<::EffekseerRendererLLGI::RendererImplemented*>(renderer);
-	auto g = static_cast<LLGI::GraphicsSwitch*>(r->GetGraphics());
+	auto g = static_cast<LLGI::GraphicsDX12*>(r->GetGraphics());
 	g->WaitFinish();
 }
 
@@ -150,167 +186,5 @@ void ExecuteCommandList(EffekseerRenderer::CommandList* commandList)
 	auto c = static_cast<EffekseerRendererLLGI::CommandList*>(commandList);
 	c->GetGraphics()->Execute(c->GetInternal());
 }
-
-#if 0
-
-RendererImplemented::RendererImplemented(int32_t squareMaxCount) : ::EffekseerRendererLLGI::RendererImplemented(squareMaxCount) {}
-
-RendererImplemented::~RendererImplemented() {}
-
-void RendererImplemented::OnLostDevice() { ::EffekseerRendererLLGI::RendererImplemented::OnLostDevice(); }
-
-void RendererImplemented::OnResetDevice() { ::EffekseerRendererLLGI::RendererImplemented::OnResetDevice(); }
-
-void RendererImplemented::Destroy() { ::EffekseerRendererLLGI::RendererImplemented::Destroy(); }
-
-void RendererImplemented::SetRestorationOfStatesFlag(bool flag)
-{
-	::EffekseerRendererLLGI::RendererImplemented::SetRestorationOfStatesFlag(flag);
-}
-
-bool RendererImplemented::BeginRendering() { return ::EffekseerRendererLLGI::RendererImplemented::BeginRendering(); }
-
-bool RendererImplemented::EndRendering() { return ::EffekseerRendererLLGI::RendererImplemented::EndRendering(); }
-
-const ::Effekseer::Vector3D& RendererImplemented::GetLightDirection() const
-{
-	return ::EffekseerRendererLLGI::RendererImplemented::GetLightDirection();
-}
-
-void RendererImplemented::SetLightDirection(const ::Effekseer::Vector3D& direction)
-{
-	::EffekseerRendererLLGI::RendererImplemented::SetLightDirection(direction);
-}
-
-const ::Effekseer::Color& RendererImplemented::GetLightColor() const
-{
-	return ::EffekseerRendererLLGI::RendererImplemented::GetLightColor();
-}
-
-void RendererImplemented::SetLightColor(const ::Effekseer::Color& color)
-{
-	::EffekseerRendererLLGI::RendererImplemented::SetLightColor(color);
-}
-
-const ::Effekseer::Color& RendererImplemented::GetLightAmbientColor() const
-{
-	return ::EffekseerRendererLLGI::RendererImplemented::GetLightAmbientColor();
-}
-
-void RendererImplemented::SetLightAmbientColor(const ::Effekseer::Color& color)
-{
-	::EffekseerRendererLLGI::RendererImplemented::SetLightAmbientColor(color);
-}
-
-int32_t RendererImplemented::GetSquareMaxCount() const { return ::EffekseerRendererLLGI::RendererImplemented::GetSquareMaxCount(); }
-
-const ::Effekseer::Matrix44& RendererImplemented::GetProjectionMatrix() const
-{
-	return ::EffekseerRendererLLGI::RendererImplemented::GetProjectionMatrix();
-}
-
-void RendererImplemented::SetProjectionMatrix(const ::Effekseer::Matrix44& mat)
-{
-	::EffekseerRendererLLGI::RendererImplemented::SetProjectionMatrix(mat);
-}
-
-const ::Effekseer::Matrix44& RendererImplemented::GetCameraMatrix() const
-{
-	return ::EffekseerRendererLLGI::RendererImplemented::GetCameraMatrix();
-}
-
-void RendererImplemented::SetCameraMatrix(const ::Effekseer::Matrix44& mat)
-{
-	::EffekseerRendererLLGI::RendererImplemented::SetCameraMatrix(mat);
-}
-
-::Effekseer::Matrix44& RendererImplemented::GetCameraProjectionMatrix()
-{
-	return ::EffekseerRendererLLGI::RendererImplemented::GetCameraProjectionMatrix();
-}
-
-::Effekseer::Vector3D RendererImplemented::GetCameraFrontDirection() const
-{
-	return ::EffekseerRendererLLGI::RendererImplemented::GetCameraFrontDirection();
-}
-
-::Effekseer::Vector3D RendererImplemented::GetCameraPosition() const
-{
-	return ::EffekseerRendererLLGI::RendererImplemented::GetCameraPosition();
-}
-
-void RendererImplemented::SetCameraParameter(const ::Effekseer::Vector3D& front, const ::Effekseer::Vector3D& position)
-{
-	::EffekseerRendererLLGI::RendererImplemented::SetCameraParameter(front, position);
-}
-
-::Effekseer::SpriteRenderer* RendererImplemented::CreateSpriteRenderer()
-{
-	return ::EffekseerRendererLLGI::RendererImplemented::CreateSpriteRenderer();
-}
-
-::Effekseer::RibbonRenderer* RendererImplemented::CreateRibbonRenderer()
-{
-	return ::EffekseerRendererLLGI::RendererImplemented::CreateRibbonRenderer();
-}
-
-::Effekseer::RingRenderer* RendererImplemented::CreateRingRenderer()
-{
-	return ::EffekseerRendererLLGI::RendererImplemented::CreateRingRenderer();
-}
-
-::Effekseer::ModelRenderer* RendererImplemented::CreateModelRenderer()
-{
-	return ::EffekseerRendererLLGI::RendererImplemented::CreateModelRenderer();
-}
-
-::Effekseer::TrackRenderer* RendererImplemented::CreateTrackRenderer()
-{
-	return ::EffekseerRendererLLGI::RendererImplemented::CreateTrackRenderer();
-}
-
-::Effekseer::TextureLoader* RendererImplemented::CreateTextureLoader(::Effekseer::FileInterface* fileInterface)
-{
-	return ::EffekseerRendererLLGI::RendererImplemented::CreateTextureLoader(fileInterface);
-}
-
-::Effekseer::ModelLoader* RendererImplemented::CreateModelLoader(::Effekseer::FileInterface* fileInterface)
-{
-	return ::EffekseerRendererLLGI::RendererImplemented::CreateModelLoader(fileInterface);
-}
-
-::Effekseer::MaterialLoader* RendererImplemented::CreateMaterialLoader(::Effekseer::FileInterface* fileInterface)
-{
-	return ::EffekseerRendererLLGI::RendererImplemented::CreateMaterialLoader(fileInterface);
-}
-
-void RendererImplemented::ResetRenderState() { return ::EffekseerRendererLLGI::RendererImplemented::ResetRenderState(); }
-
-EffekseerRenderer::DistortingCallback* RendererImplemented::GetDistortingCallback()
-{
-	return ::EffekseerRendererLLGI::RendererImplemented::GetDistortingCallback();
-}
-
-void RendererImplemented::SetDistortingCallback(EffekseerRenderer::DistortingCallback* callback)
-{
-	::EffekseerRendererLLGI::RendererImplemented::SetDistortingCallback(callback);
-}
-
-int32_t RendererImplemented::GetDrawCallCount() const { return ::EffekseerRendererLLGI::RendererImplemented::GetDrawCallCount(); }
-
-int32_t RendererImplemented::GetDrawVertexCount() const { return ::EffekseerRendererLLGI::RendererImplemented::GetDrawVertexCount(); }
-
-void RendererImplemented::ResetDrawCallCount() { ::EffekseerRendererLLGI::RendererImplemented::ResetDrawCallCount(); }
-
-void RendererImplemented::ResetDrawVertexCount() { ::EffekseerRendererLLGI::RendererImplemented::ResetDrawVertexCount(); }
-
-void RendererImplemented::SetRenderMode(Effekseer::RenderMode renderMode)
-{
-	::EffekseerRendererLLGI::RendererImplemented::SetRenderMode(renderMode);
-}
-
-Effekseer::RenderMode RendererImplemented::GetRenderMode() { return ::EffekseerRendererLLGI::RendererImplemented::GetRenderMode(); }
-
-#endif
 
 } // namespace EffekseerRendererDX12
