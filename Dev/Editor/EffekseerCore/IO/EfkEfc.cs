63010236b5167e7c5856a744a19437d4f9020168
durswd <swda.durl@gmail.com>
2019-09-11 23:46:25 +0900

    initial commit

---
+++ Dev/Editor/EffekseerCore/IO/EfkEfc.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Effekseer.IO
+{
+	class EfkEfc
+	{
+		public bool Save()
+		{
+			return false;
+		}
+
+		public bool Load()
+		{
+			return false;
+		}
+	}
+}
