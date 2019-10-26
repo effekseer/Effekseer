4fcce5568a5bfcae1eaa267df1f546f0107c5e53
durswd <swda.durl@gmail.com>
2019-10-26 17:00:22 +0900

    typo and fixed scripts

--- Script/setup.py
+++ Script/setup.py
@@ -3,5 +3,5 @@ import os
 
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
-shutil.rmtree('Dev/release/resources/fonts', ignore_errors=True)
+shutil.rmtree('../Dev/release/resources/fonts', ignore_errors=True)
 shutil.copytree('../ResourceData/tool/resources/fonts', '../Dev/release/resources/fonts')
