2249f8acd6f6e12ba232ae771d074adf158550f9
lriki <lriki.net@gmail.com>
2019-11-10 15:25:32 +0900

    added shaders for vulkan renderer

--- Dev/Cpp/EffekseerRendererVulkan/EffekseerRenderer/EffekseerRendererVulkan.Renderer.cpp
+++ Dev/Cpp/EffekseerRendererVulkan/EffekseerRenderer/EffekseerRendererVulkan.Renderer.cpp
@@ -6,6 +6,45 @@
 namespace EffekseerRendererVulkan
 {
 
+
+static void CreateFixedShaderForVulkan(EffekseerRendererLLGI::FixedShader* shader)
+{
+    assert(shader);
+    if (!shader) return;
+
+    static const std::vector<uint8_t> standard_vert = {
+#include "Shader/Vulkan/standard.vert.spv.inl"
+    };
+    shader->StandardTexture_VS = { { standard_vert.data(), (int32_t)standard_vert.size() } };
+    //shader->Standard_VS = { { standard_vert.data(), standard_vert.size() } };
+
+    static const std::vector<uint8_t> standard_distorted_vert = {
+#include "Shader/Vulkan/standard_distortion.vert.spv.inl"
+    };
+    shader->StandardDistortedTexture_VS = { { standard_distorted_vert.data(), (int32_t)standard_distorted_vert.size() } };
+    //shader->StandardDistorted_VS = { { standard_distorted_vert.data(), standard_distorted_vert.size() } };
+
+    static const std::vector<uint8_t> standard_frag = {
+#include "Shader/Vulkan/standard.frag.spv.inl"
+    };
+    shader->StandardTexture_PS = { { standard_frag.data(), (int32_t)standard_frag.size() } };
+
+    static const std::vector<uint8_t> standard_no_texture_frag = {
+#include "Shader/Vulkan/standard_no_texture.frag.spv.inl"
+    };
+    //shader->Standard_PS = { { standard_no_texture_frag.data(), standard_no_texture_frag.size() } };
+
+    static const std::vector<uint8_t> standard_distortion_frag = {
+#include "Shader/Vulkan/standard_distortion.frag.spv.inl"
+    };
+    shader->StandardDistortedTexture_PS = { { standard_distortion_frag.data(), (int32_t)standard_distortion_frag.size() } };
+
+    static const std::vector<uint8_t> standard_distortion_no_texture_frag = {
+#include "Shader/Vulkan/standard_distortion_no_texture.frag.spv.inl"
+    };
+    //shader->StandardDistorted_PS = { { standard_distortion_no_texture_frag.data(), standard_distortion_no_texture_frag.size() } };
+}
+
 ::EffekseerRenderer::Renderer* Create(int32_t squareMaxCount)
 {
     throw "Not implemented.";
---
+++ Dev/Cpp/EffekseerRendererVulkan/EffekseerRenderer/Shader/Vulkan/Radme.md
@@ -0,0 +1,17 @@
+
+These files are shader code for EffekseerRendererLLGI (Vulkan device).
+
+Execute the following command to compile.
+
+```
+python compile.py
+```
+
+### Note
+LLGI interface (3 Jul 2019):
+
+- vert : set=0
+- frag : set=1
+- UBO : binding=0
+- Texture: binding=1~
+
