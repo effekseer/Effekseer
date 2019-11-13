bc717929e94fc0301f62998ecd1acded0b81c689
durswd <swda.durl@gmail.com>
2019-11-14 01:21:04 +0900

    updated submodules

--- Script/setup.py
+++ Script/setup.py
@@ -1,7 +1,18 @@
 import shutil
 import os
+import platform
 
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
 shutil.rmtree('../Dev/release/resources/fonts', ignore_errors=True)
 shutil.copytree('../ResourceData/tool/resources/fonts', '../Dev/release/resources/fonts')
+
+pf = platform.system()
+if pf == 'Windows':
+    shutil.copyfile("../ResourceData/tool/tools/win/fbxToEffekseerModelConverter.exe", "../Dev/release/tools/fbxToEffekseerModelConverter.exe")
+
+if pf == 'Darwin':
+    shutil.copyfile("../ResourceData/tool/tools/mac/fbxToEffekseerModelConverter", "../Dev/release/tools/fbxToEffekseerModelConverter")
+    shutil.copyfile("../ResourceData/tool/tools/mac/libfbxsdk.dylib", "../Dev/release/tools/libfbxsdk.dylib")
+
+    
