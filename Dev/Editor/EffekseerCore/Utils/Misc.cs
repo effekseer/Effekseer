26aa55f632b448511f3e4d4a6fce090b757d3f76
durswd <swda.durl@gmail.com>
2019-11-27 09:25:30 +0900

    Fix a bug that fails escaping space

---
+++ Dev/Editor/EffekseerCore/Utils/Misc.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Effekseer.Utils
+{
+	public class Misc
+	{
+		public static string GetRelativePath(string basePath, string path)
+		{
+			Func<string, string> escape = (string s) =>
+			{
+				return s.Replace("%", "%25");
+			};
+
+			Func<string, string> unescape = (string s) =>
+			{
+				return s.Replace("%25", "%");
+			};
+
+			Uri basepath = new Uri(escape(basePath));
+			Uri targetPath = new Uri(escape(path));
+			return unescape(Uri.UnescapeDataString(basepath.MakeRelativeUri(targetPath).ToString()));
+		}
+		public static string GetAbsolutePath(string basePath, string path)
+		{
+			Func<string, string> escape = (string s) =>
+			{
+				return s.Replace("%", "%25");
+			};
+
+			Func<string, string> unescape = (string s) =>
+			{
+				return s.Replace("%25", "%");
+			};
+
+			Uri escaped = new Uri(escape(basePath));
+			Uri targetPath = new Uri(escaped, escape(path));
+			return unescape(Uri.UnescapeDataString(targetPath.LocalPath));
+		}
+	}
+}
