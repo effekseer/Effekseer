f9439989172f324c77b8e4b2e449bb33bca57005
eorfeorf <orerrurirar2@gmail.com>
2019-12-16 23:37:26 +0900

    Fixed GetAbsolutePath.

--- Dev/Editor/EffekseerCore/Utils/Misc.cs
+++ Dev/Editor/EffekseerCore/Utils/Misc.cs
@@ -20,25 +20,33 @@ namespace Effekseer.Utils
 				return s.Replace("%25", "%");
 			};
 
-			Uri basepath = new Uri(escape(basePath));
-			Uri targetPath = new Uri(escape(path));
-			return unescape(Uri.UnescapeDataString(basepath.MakeRelativeUri(targetPath).ToString()));
+			var basePath_esc = escape(basePath);
+			var path_ecs = escape(path);
+			Uri basepath = new Uri(basePath_esc);
+			Uri targetPath = new Uri(path_ecs);
+			var uriPath = Uri.UnescapeDataString(basepath.MakeRelativeUri(targetPath).ToString());
+			var retPath = unescape(uriPath);
+			return retPath;
 		}
 		public static string GetAbsolutePath(string basePath, string path)
 		{
-			Func<string, string> escape = (string s) =>
+			Func<string, string> doubleBackslashToSlash = (string s) =>
 			{
-				return s.Replace("%", "%25");
+				string input = s;
+				string pattern = @"\\";
+				string replacement = @"/";
+				string result = System.Text.RegularExpressions.Regex.Replace(input, pattern, replacement);
+				return result;
 			};
 
-			Func<string, string> unescape = (string s) =>
-			{
-				return s.Replace("%25", "%");
-			};
-
-			Uri escaped = new Uri(escape(basePath));
-			Uri targetPath = new Uri(escaped, escape(path));
-			return unescape(Uri.UnescapeDataString(targetPath.LocalPath));
+			var basePath_ecs = new Uri(basePath, UriKind.Relative);
+			var path_ecs = new Uri(path, UriKind.Relative);
+			var basePath_slash = doubleBackslashToSlash(basePath_ecs.ToString());
+			var basePath_uri = new Uri(basePath_slash, UriKind.Absolute);
+			var path_uri = new Uri(path_ecs.ToString(), UriKind.Relative);
+			var targetPath = new Uri(basePath_uri, path_uri);
+			var ret = targetPath.LocalPath.ToString();
+			return ret;
 		}
 	}
 }
