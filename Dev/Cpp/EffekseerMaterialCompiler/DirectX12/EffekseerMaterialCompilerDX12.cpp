1026056a293520285e7704ff97fcdcd5d22a9146
durswd <swda.durl@gmail.com>
2020-01-12 14:08:54 +0900

    Refactoring

--- Dev/Cpp/EffekseerMaterialCompiler/DirectX12/EffekseerMaterialCompilerDX12.cpp
+++ Dev/Cpp/EffekseerMaterialCompiler/DirectX12/EffekseerMaterialCompilerDX12.cpp
@@ -45,428 +45,8 @@ void Serialize(std::vector<uint8_t>& dst, const LLGI::CompilerResult& result) {
 namespace DX12
 {
 
-static char* material_common_define = R"(
-#define MOD fmod
-#define FRAC frac
-#define LERP lerp
-
-)";
-
-static char* material_common_vs_functions = R"(
-
-float2 GetUV(float2 uv)
-{
-	uv.y = mUVInversed.x + mUVInversed.y * uv.y;
-	return uv;
-}
-
-float2 GetUVBack(float2 uv)
-{
-	uv.y = mUVInversed.z + mUVInversed.w * uv.y;
-	return uv;
-}
-
-)";
-
-static char* material_sprite_vs_pre_simple = R"(
-
-struct VS_Input
-{
-	float3 Pos		: POSITION0;
-	float4 Color		: NORMAL0;
-	float2 UV		: TEXCOORD0;
-};
-
-struct VS_Output
-{
-	float4 Position		: SV_POSITION;
-	float4 VColor		: COLOR;
-	float2 UV1		: TEXCOORD0;
-	float2 UV2		: TEXCOORD1;
-	float3 WorldP	: TEXCOORD2;
-	float3 WorldN : TEXCOORD3;
-	float3 WorldT : TEXCOORD4;
-	float3 WorldB : TEXCOORD5;
-	float2 ScreenUV : TEXCOORD6;
-};
-
-cbuffer VSConstantBuffer : register(b0) {
-
-float4x4 mCamera		: register(c0);
-float4x4 mProj			: register(c4);
-float4 mUVInversed		: register(c8);
-float4 predefined_uniform : register(c9);
-
-)";
-
-static char* material_sprite_vs_pre = R"(
-
-struct VS_Input
-{
-	float3 Pos		: POSITION0;
-	float4 Color		: NORMAL0;
-	float4 Normal		: NORMAL1;
-	float4 Tangent		: NORMAL2;
-	float2 UV1		: TEXCOORD0;
-	float2 UV2		: TEXCOORD1;
-	//$C_IN1$
-	//$C_IN2$
-};
-
-struct VS_Output
-{
-	float4 Position		: SV_POSITION;
-	float4 VColor		: COLOR;
-	float2 UV1		: TEXCOORD0;
-	float2 UV2		: TEXCOORD1;
-	float3 WorldP	: TEXCOORD2;
-	float3 WorldN : TEXCOORD3;
-	float3 WorldT : TEXCOORD4;
-	float3 WorldB : TEXCOORD5;
-	float2 ScreenUV : TEXCOORD6;
-	//$C_OUT1$
-	//$C_OUT2$
-};
-
-cbuffer VSConstantBuffer : register(b0) {
-
-float4x4 mCamera		: register(c0);
-float4x4 mProj			: register(c4);
-float4 mUVInversed		: register(c8);
-float4 predefined_uniform : register(c9);
-
-)";
-
-static char* material_sprite_vs_suf1_simple = R"(
-
-
-VS_Output main( const VS_Input Input )
-{
-	VS_Output Output = (VS_Output)0;
-	float4 worldPos = { Input.Pos.x, Input.Pos.y, Input.Pos.z, 1.0 };
-	float3 worldNormal = float3(0.0, 0.0, 0.0);
-	float3 worldBinormal = float3(0.0, 0.0, 0.0);
-	float3 worldTangent = float3(0.0, 0.0, 0.0);
-
-	// UV
-	float uv1 = Input.UV;
-	float uv2 = Input.UV;
-	//uv1.y = mUVInversed.x + mUVInversed.y * uv1.y;
-	//uv2.y = mUVInversed.x + mUVInversed.y * uv2.y;
-
-	// NBT
-	Output.WorldN = worldNormal;
-	Output.WorldB = worldBinormal;
-	Output.WorldT = worldTangent;
-
-	float3 pixelNormalDir = worldNormal;
-	float4 vcolor = Input.Color;
-)";
-
-static char* material_sprite_vs_suf1 = R"(
-
-VS_Output main( const VS_Input Input )
-{
-	VS_Output Output = (VS_Output)0;
-	float3 worldPos = Input.Pos;
-	float3 worldNormal = (Input.Normal - float3(0.5, 0.5, 0.5)) * 2.0;
-	float3 worldTangent = (Input.Tangent - float3(0.5, 0.5, 0.5)) * 2.0;
-	float3 worldBinormal = cross(worldNormal, worldTangent);
-
-	// UV
-	float2 uv1 = Input.UV1;
-	float2 uv2 = Input.UV2;
-	//uv1.y = mUVInversed.x + mUVInversed.y * uv1.y;
-	//uv2.y = mUVInversed.x + mUVInversed.y * uv2.y;
-
-	// NBT
-	Output.WorldN = worldNormal;
-	Output.WorldB = worldBinormal;
-	Output.WorldT = worldTangent;
-
-	float3 pixelNormalDir = worldNormal;
-	float4 vcolor = Input.Color;
-)";
-
-static char* material_sprite_vs_suf2 = R"(
-
-	worldPos = worldPos + worldPositionOffset;
-
-	float4 cameraPos = mul(mCamera, float4(worldPos, 1.0));
-	cameraPos = cameraPos / cameraPos.w;
-	Output.Position = mul(mProj, cameraPos);
-
-	Output.WorldP = worldPos;
-	Output.VColor = Input.Color;
-	Output.UV1 = uv1;
-	Output.UV2 = uv2;
-	Output.ScreenUV = Output.Position.xy / Output.Position.w;
-	Output.ScreenUV.xy = float2(Output.ScreenUV.x + 1.0, 1.0 - Output.ScreenUV.y) * 0.5;
-
-	return Output;
-}
-
-)";
-
-static char* model_vs_pre = R"(
-
-struct VS_Input
-{
-	float3 Pos		: POSITION0;
-	float3 Normal		: NORMAL0;
-	float3 Binormal		: NORMAL1;
-	float3 Tangent		: NORMAL2;
-	float2 UV		: TEXCOORD0;
-	float4 Color		: NORMAL3;
-	uint4 Index		: BLENDINDICES0;
-
-};
-
-struct VS_Output
-{
-	float4 Position		: SV_POSITION;
-	float4 VColor		: COLOR;
-	float2 UV1		: TEXCOORD0;
-	float2 UV2		: TEXCOORD1;
-	float3 WorldP	: TEXCOORD2;
-	float3 WorldN : TEXCOORD3;
-	float3 WorldT : TEXCOORD4;
-	float3 WorldB : TEXCOORD5;
-	float2 ScreenUV : TEXCOORD6;
-	//$C_OUT1$
-	//$C_OUT2$
-};
-
-cbuffer VSConstantBuffer : register(b0) {
-
-float4x4 mCameraProj		: register( c0 );
-float4x4 mModel[1]		: register( c4 );
-float4	fUV[1]			: register( c8 );
-float4	fModelColor[1]		: register( c9 );
-
-float4 mUVInversed		: register(c10);
-float4 predefined_uniform : register(c11);
-
-// custom1
-// custom2
-
-)";
-
-static char* model_vs_suf1 = R"(
-
-
-VS_Output main( const VS_Input Input )
-{
-	float4x4 matModel = mModel[Input.Index.x];
-	float4 uv = fUV[Input.Index.x];
-	float4 modelColor = fModelColor[Input.Index.x] * Input.Color;
-
-	VS_Output Output = (VS_Output)0;
-	float4 localPosition = { Input.Pos.x, Input.Pos.y, Input.Pos.z, 1.0 }; 
-
-	float3x3 matRotModel = (float3x3)matModel;
-
-	float3 worldPos = mul( matModel, localPosition ).xyz;
-	float3 worldNormal = normalize( mul( matRotModel, Input.Normal ) );
-	float3 worldBinormal = normalize( mul( matRotModel, Input.Binormal ) );
-	float3 worldTangent = normalize( mul( matRotModel, Input.Tangent ) );
-
-	float2 uv1;
-	uv1.x = Input.UV.x * uv.z + uv.x;
-	uv1.y = Input.UV.y * uv.w + uv.y;
-	float2 uv2 = Input.UV;
-
-	//uv1.y = mUVInversed.x + mUVInversed.y * uv1.y;
-	//uv2.y = mUVInversed.x + mUVInversed.y * uv2.y;
-
-	float3 pixelNormalDir = worldNormal;
-	float4 vcolor = modelColor;
-)";
-
-static char* model_vs_suf2 = R"(
-
-	worldPos = worldPos + worldPositionOffset;
-
-	Output.Position = mul( mCameraProj,  float4(worldPos, 1.0) );
-
-	Output.WorldP = worldPos;
-	Output.WorldN = worldNormal;
-	Output.WorldB = worldBinormal;
-	Output.WorldT = worldTangent;
-
-	Output.VColor = modelColor;
-	Output.UV1 = uv1;
-	Output.UV2 = uv2;
-	Output.ScreenUV = Output.Position.xy / Output.Position.w;
-	Output.ScreenUV.xy = float2(Output.ScreenUV.x + 1.0, 1.0 - Output.ScreenUV.y) * 0.5;
-
-	return Output;
-}
-
-
-)";
-
-static char* g_material_ps_pre = R"(
-
-struct PS_Input
-{
-	float4 Position		: SV_POSITION;
-	float4 VColor		: COLOR;
-	float2 UV1		: TEXCOORD0;
-	float2 UV2		: TEXCOORD1;
-	float3 WorldP	: TEXCOORD2;
-	float3 WorldN : TEXCOORD3;
-	float3 WorldT : TEXCOORD4;
-	float3 WorldB : TEXCOORD5;
-	float2 ScreenUV : TEXCOORD6;
-	//$C_PIN1$
-	//$C_PIN2$
-};
-
-#ifdef _DIRECTX11
-cbuffer PSConstantBuffer : register(b0) {
-#else
-cbuffer PSConstantBuffer : register(b1) {
-#endif
-
-)";
-
-static char* g_material_ps_suf1 = R"(
-
-float2 GetUV(float2 uv)
-{
-	uv.y = mUVInversedBack.x + mUVInversedBack.y * uv.y;
-	return uv;
-}
-
-float2 GetUVBack(float2 uv)
-{
-	uv.y = mUVInversedBack.z + mUVInversedBack.w * uv.y;
-	return uv;
-}
-
-#ifdef _MATERIAL_LIT_
-
-#define lightScale 3.14
-
-float calcD_GGX(float roughness, float dotNH)
-{
-	float alpha = roughness*roughness;
-	float alphaSqr = alpha*alpha;
-	float pi = 3.14159;
-	float denom = dotNH * dotNH *(alphaSqr-1.0) + 1.0;
-	return (alpha / denom) * (alpha / denom) / pi;
-}
-
-float calcF(float F0, float dotLH)
-{
-	float dotLH5 = pow(1.0-dotLH,5.0);
-	return F0 + (1.0-F0)*(dotLH5);
-}
-
-float calcG_Schlick(float roughness, float dotNV, float dotNL)
-{
-	// UE4
-	float k = (roughness + 1.0) * (roughness + 1.0) / 8.0;
-	// float k = roughness * roughness / 2.0;
-
-	float gV = dotNV*(1.0 - k) + k;
-	float gL = dotNL*(1.0 - k) + k;
-
-	return 1.0 / (gV * gL);
-}
-
-float calcLightingGGX(float3 N, float3 V, float3 L, float roughness, float F0)
-{
-	float3 H = normalize(V+L);
-
-	float dotNL = saturate( dot(N,L) );
-	float dotLH = saturate( dot(L,H) );
-	float dotNH = saturate( dot(N,H) ) - 0.001;
-	float dotNV = saturate( dot(N,V) ) + 0.001;
-
-	float D = calcD_GGX(roughness, dotNH);
-	float F = calcF(F0, dotLH);
-	float G = calcG_Schlick(roughness, dotNV, dotNL);
-
-	return dotNL * D * F * G / 4.0;
-}
-
-float3 calcDirectionalLightDiffuseColor(float3 diffuseColor, float3 normal, float3 lightDir, float ao)
-{
-	float3 color = float3(0.0,0.0,0.0);
-
-	float NoL = dot(normal,lightDir);
-	color.xyz = lightColor.xyz * lightScale * max(NoL,0.0) * ao / 3.14;
-	color.xyz = color.xyz * diffuseColor.xyz;
-	return color;
-}
-
-#endif
-
-float4 main( const PS_Input Input ) : SV_Target
-{
-	float2 uv1 = Input.UV1;
-	float2 uv2 = Input.UV2;
-	float3 worldPos = Input.WorldP;
-	float3 worldNormal = Input.WorldN;
-	float3 worldBinormal = Input.WorldB;
-	float3 worldTangent = Input.WorldT;
-
-	float3 pixelNormalDir = worldNormal;
-	float4 vcolor = Input.VColor;
-)";
-
-static char* g_material_ps_suf2_unlit = R"(
-
-
-	float4 Output = float4(emissive, opacity);
-
-	if(opacityMask <= 0.0f) discard;
-	if(opacity <= 0.0) discard;
-
-	return Output;
-}
-
-)";
-
-static char* g_material_ps_suf2_lit = R"(
-	float3 viewDir = normalize(cameraPosition.xyz - worldPos);
-	float3 diffuse = calcDirectionalLightDiffuseColor(baseColor, pixelNormalDir, lightDirection.xyz, ambientOcclusion);
-	float3 specular = lightColor.xyz * lightScale * calcLightingGGX(worldNormal, viewDir, lightDirection.xyz, roughness, 0.9);
-
-	float4 Output =  float4(metallic * specular + (1.0 - metallic) * diffuse, opacity);
-	Output.xyz = Output.xyz + emissive.xyz;
-
-	if(opacityMask <= 0.0) discard;
-	if(opacity <= 0.0) discard;
-
-	return Output;
-}
-
-)";
-
-static char* g_material_ps_suf2_refraction = R"(
-
-	float airRefraction = 1.0;
-	float3 dir = mul((float3x3)cameraMat, pixelNormalDir);
-	dir.y = -dir.y;
-
-	float2 distortUV = 	dir.xy * (refraction - airRefraction);
-
-	distortUV += Input.ScreenUV;
-	distortUV = GetUVBack(distortUV);	
-
-	float4 bg = background_texture.Sample(background_sampler, distortUV);
-	float4 Output = bg;
-
-	if(opacityMask <= 0.0) discard;
-	if(opacity <= 0.0) discard;
-
-	return Output;
-}
-
-)";
+#define _DIRECTX12 1
+#include "../HLSL/HLSL.h"
 
 std::string Replace(std::string target, std::string from_, std::string to_)
 {
@@ -528,7 +108,7 @@ void ExportTexture(std::ostringstream& maincode, const char* name, int32_t regis
 	maincode << "SamplerState " << name << "_sampler : register(s" << registerId << ");" << std::endl;
 }
 
-int32_t ExportHeader(std::ostringstream& maincode, Material* material, int stage, bool isSprite)
+int32_t ExportHeader(std::ostringstream& maincode, Material* material, int stage, bool isSprite, int instanceCount)
 {
 	auto cind = 0;
 
@@ -551,7 +131,7 @@ int32_t ExportHeader(std::ostringstream& maincode, Material* material, int stage
 		else
 		{
 			maincode << model_vs_pre;
-			cind = 246;
+			cind = 6 + instanceCount * 6;
 		}
 	}
 	else
@@ -663,7 +243,7 @@ void ExportMain(
 }
 
 ShaderData
-GenerateShader(Material* material, MaterialShaderType shaderType, int32_t maximumTextureCount, int32_t pixelShaderTextureSlotOffset)
+GenerateShader(Material* material, MaterialShaderType shaderType, int32_t maximumTextureCount, int32_t pixelShaderTextureSlotOffset, int32_t instanceCount)
 {
 	ShaderData shaderData;
 
@@ -675,7 +255,7 @@ GenerateShader(Material* material, MaterialShaderType shaderType, int32_t maximu
 	{
 		std::ostringstream maincode;
 
-		auto cind = ExportHeader(maincode, material, stage, isSprite);
+		auto cind = ExportHeader(maincode, material, stage, isSprite, instanceCount);
 
 		if (stage == 1)
 		{
@@ -710,15 +290,15 @@ GenerateShader(Material* material, MaterialShaderType shaderType, int32_t maximu
 		{
 			if (material->GetCustomData1Count() > 0)
 			{
-				maincode << "float4 customData1_[1]"
+				maincode << "float4 customData1_[" << instanceCount << "]"
 						 << " : register(c" << cind << ");" << std::endl;
-				cind += 1;
+				cind += instanceCount;
 			}
 			if (material->GetCustomData2Count() > 0)
 			{
-				maincode << "float4 customData2_[1]"
+				maincode << "float4 customData2_[" << instanceCount << "]"
 						 << " : register(c" << cind << ");" << std::endl;
-				cind += 1;
+				cind += instanceCount;
 			}
 		}
 
@@ -952,7 +532,7 @@ CompiledMaterialBinary* MaterialCompilerDX12::Compile(Material* material, int32_
 	};
 
 	auto saveBinary = [&material, &binary, &convertToVectorVS, &convertToVectorPS, &maximumTextureCount](MaterialShaderType type) {
-		auto shader = DX12::GenerateShader(material, type, maximumTextureCount, 8);
+		auto shader = DX12::GenerateShader(material, type, maximumTextureCount, 8, 1);
 		binary->SetVertexShaderData(type, convertToVectorVS(shader.CodeVS));
 		binary->SetPixelShaderData(type, convertToVectorPS(shader.CodePS));
 	};
---
+++ Dev/Cpp/EffekseerMaterialCompiler/HLSL/HLSL.h
@@ -0,0 +1,443 @@
+
+static char* material_common_define = R"(
+#define MOD fmod
+#define FRAC frac
+#define LERP lerp
+)";
+
+static char* material_common_vs_functions = R"(
+
+float2 GetUV(float2 uv)
+{
+	uv.y = mUVInversed.x + mUVInversed.y * uv.y;
+	return uv;
+}
+
+float2 GetUVBack(float2 uv)
+{
+	uv.y = mUVInversed.z + mUVInversed.w * uv.y;
+	return uv;
+}
+
+)";
+
+static char* material_sprite_vs_pre_simple = R"(
+
+struct VS_Input
+{
+	float3 Pos		: POSITION0;
+	float4 Color		: NORMAL0;
+	float2 UV		: TEXCOORD0;
+};
+
+struct VS_Output
+{
+	float4 Position		: SV_POSITION;
+	float4 VColor		: COLOR;
+	float2 UV1		: TEXCOORD0;
+	float2 UV2		: TEXCOORD1;
+	float3 WorldP	: TEXCOORD2;
+	float3 WorldN : TEXCOORD3;
+	float3 WorldT : TEXCOORD4;
+	float3 WorldB : TEXCOORD5;
+	float2 ScreenUV : TEXCOORD6;
+};
+
+cbuffer VSConstantBuffer : register(b0) {
+
+float4x4 mCamera		: register(c0);
+float4x4 mProj			: register(c4);
+float4 mUVInversed		: register(c8);
+float4 predefined_uniform : register(c9);
+
+)";
+
+static char* material_sprite_vs_pre = R"(
+
+struct VS_Input
+{
+	float3 Pos		: POSITION0;
+	float4 Color		: NORMAL0;
+	float4 Normal		: NORMAL1;
+	float4 Tangent		: NORMAL2;
+	float2 UV1		: TEXCOORD0;
+	float2 UV2		: TEXCOORD1;
+	//$C_IN1$
+	//$C_IN2$
+};
+
+struct VS_Output
+{
+	float4 Position		: SV_POSITION;
+	float4 VColor		: COLOR;
+	float2 UV1		: TEXCOORD0;
+	float2 UV2		: TEXCOORD1;
+	float3 WorldP	: TEXCOORD2;
+	float3 WorldN : TEXCOORD3;
+	float3 WorldT : TEXCOORD4;
+	float3 WorldB : TEXCOORD5;
+	float2 ScreenUV : TEXCOORD6;
+	//$C_OUT1$
+	//$C_OUT2$
+};
+
+cbuffer VSConstantBuffer : register(b0) {
+
+float4x4 mCamera		: register(c0);
+float4x4 mProj			: register(c4);
+float4 mUVInversed		: register(c8);
+float4 predefined_uniform : register(c9);
+
+)";
+
+static char* material_sprite_vs_suf1_simple = R"(
+
+
+VS_Output main( const VS_Input Input )
+{
+	VS_Output Output = (VS_Output)0;
+	float4 worldPos = { Input.Pos.x, Input.Pos.y, Input.Pos.z, 1.0 };
+	float3 worldNormal = float3(0.0, 0.0, 0.0);
+	float3 worldBinormal = float3(0.0, 0.0, 0.0);
+	float3 worldTangent = float3(0.0, 0.0, 0.0);
+
+	// UV
+	float uv1 = Input.UV;
+	float uv2 = Input.UV;
+	//uv1.y = mUVInversed.x + mUVInversed.y * uv1.y;
+	//uv2.y = mUVInversed.x + mUVInversed.y * uv2.y;
+
+	// NBT
+	Output.WorldN = worldNormal;
+	Output.WorldB = worldBinormal;
+	Output.WorldT = worldTangent;
+
+	float3 pixelNormalDir = worldNormal;
+	float4 vcolor = Input.Color;
+)";
+
+static char* material_sprite_vs_suf1 = R"(
+
+VS_Output main( const VS_Input Input )
+{
+	VS_Output Output = (VS_Output)0;
+	float3 worldPos = Input.Pos;
+	float3 worldNormal = (Input.Normal - float3(0.5, 0.5, 0.5)) * 2.0;
+	float3 worldTangent = (Input.Tangent - float3(0.5, 0.5, 0.5)) * 2.0;
+	float3 worldBinormal = cross(worldNormal, worldTangent);
+
+	// UV
+	float2 uv1 = Input.UV1;
+	float2 uv2 = Input.UV2;
+	//uv1.y = mUVInversed.x + mUVInversed.y * uv1.y;
+	//uv2.y = mUVInversed.x + mUVInversed.y * uv2.y;
+
+	// NBT
+	Output.WorldN = worldNormal;
+	Output.WorldB = worldBinormal;
+	Output.WorldT = worldTangent;
+
+	float3 pixelNormalDir = worldNormal;
+	float4 vcolor = Input.Color;
+)";
+
+static char* material_sprite_vs_suf2 = R"(
+
+	worldPos = worldPos + worldPositionOffset;
+
+	float4 cameraPos = mul(mCamera, float4(worldPos, 1.0));
+	cameraPos = cameraPos / cameraPos.w;
+	Output.Position = mul(mProj, cameraPos);
+
+	Output.WorldP = worldPos;
+	Output.VColor = Input.Color;
+	Output.UV1 = uv1;
+	Output.UV2 = uv2;
+	Output.ScreenUV = Output.Position.xy / Output.Position.w;
+	Output.ScreenUV.xy = float2(Output.ScreenUV.x + 1.0, 1.0 - Output.ScreenUV.y) * 0.5;
+
+	return Output;
+}
+
+)";
+
+static char* model_vs_pre = R"(
+
+struct VS_Input
+{
+	float3 Pos		: POSITION0;
+	float3 Normal		: NORMAL0;
+	float3 Binormal		: NORMAL1;
+	float3 Tangent		: NORMAL2;
+	float2 UV		: TEXCOORD0;
+	float4 Color		: NORMAL3;
+	uint4 Index		: BLENDINDICES0;
+
+};
+
+struct VS_Output
+{
+	float4 Position		: SV_POSITION;
+	float4 VColor		: COLOR;
+	float2 UV1		: TEXCOORD0;
+	float2 UV2		: TEXCOORD1;
+	float3 WorldP	: TEXCOORD2;
+	float3 WorldN : TEXCOORD3;
+	float3 WorldT : TEXCOORD4;
+	float3 WorldB : TEXCOORD5;
+	float2 ScreenUV : TEXCOORD6;
+	//$C_OUT1$
+	//$C_OUT2$
+};
+
+cbuffer VSConstantBuffer : register(b0) {
+
+)"
+
+#ifdef _DIRECTX11
+R"(
+float4x4 mCameraProj		: register( c0 );
+float4x4 mModel[40]		: register( c4 );
+float4	fUV[40]			: register( c164 );
+float4	fModelColor[40]		: register( c204 );
+
+float4 mUVInversed		: register(c244);
+float4 predefined_uniform : register(c245);
+)"
+#else
+R"(
+float4x4 mCameraProj		: register( c0 );
+float4x4 mModel[1]		: register( c4 );
+float4	fUV[1]			: register( c8 );
+float4	fModelColor[1]		: register( c9 );
+
+float4 mUVInversed		: register(c10);
+float4 predefined_uniform : register(c11);
+)"
+#endif
+
+R"(
+// custom1
+// custom2
+)";
+
+static char* model_vs_suf1 = R"(
+
+
+VS_Output main( const VS_Input Input )
+{
+	float4x4 matModel = mModel[Input.Index.x];
+	float4 uv = fUV[Input.Index.x];
+	float4 modelColor = fModelColor[Input.Index.x] * Input.Color;
+
+	VS_Output Output = (VS_Output)0;
+	float4 localPosition = { Input.Pos.x, Input.Pos.y, Input.Pos.z, 1.0 }; 
+
+	float3x3 matRotModel = (float3x3)matModel;
+
+	float3 worldPos = mul( matModel, localPosition ).xyz;
+	float3 worldNormal = normalize( mul( matRotModel, Input.Normal ) );
+	float3 worldBinormal = normalize( mul( matRotModel, Input.Binormal ) );
+	float3 worldTangent = normalize( mul( matRotModel, Input.Tangent ) );
+
+	float2 uv1;
+	uv1.x = Input.UV.x * uv.z + uv.x;
+	uv1.y = Input.UV.y * uv.w + uv.y;
+	float2 uv2 = Input.UV;
+
+	//uv1.y = mUVInversed.x + mUVInversed.y * uv1.y;
+	//uv2.y = mUVInversed.x + mUVInversed.y * uv2.y;
+
+	float3 pixelNormalDir = worldNormal;
+	float4 vcolor = modelColor;
+)";
+
+static char* model_vs_suf2 = R"(
+
+	worldPos = worldPos + worldPositionOffset;
+
+	Output.Position = mul( mCameraProj,  float4(worldPos, 1.0) );
+
+	Output.WorldP = worldPos;
+	Output.WorldN = worldNormal;
+	Output.WorldB = worldBinormal;
+	Output.WorldT = worldTangent;
+
+	Output.VColor = modelColor;
+	Output.UV1 = uv1;
+	Output.UV2 = uv2;
+	Output.ScreenUV = Output.Position.xy / Output.Position.w;
+	Output.ScreenUV.xy = float2(Output.ScreenUV.x + 1.0, 1.0 - Output.ScreenUV.y) * 0.5;
+
+	return Output;
+}
+
+
+)";
+
+static char* g_material_ps_pre = R"(
+
+struct PS_Input
+{
+	float4 Position		: SV_POSITION;
+	float4 VColor		: COLOR;
+	float2 UV1		: TEXCOORD0;
+	float2 UV2		: TEXCOORD1;
+	float3 WorldP	: TEXCOORD2;
+	float3 WorldN : TEXCOORD3;
+	float3 WorldT : TEXCOORD4;
+	float3 WorldB : TEXCOORD5;
+	float2 ScreenUV : TEXCOORD6;
+	//$C_PIN1$
+	//$C_PIN2$
+};
+)"
+
+#ifdef _DIRECTX11
+R"(
+cbuffer PSConstantBuffer : register(b0) {
+)";
+
+#else
+R"(
+cbuffer PSConstantBuffer : register(b1) {
+)";
+
+#endif
+
+static char* g_material_ps_suf1 = R"(
+
+float2 GetUV(float2 uv)
+{
+	uv.y = mUVInversedBack.x + mUVInversedBack.y * uv.y;
+	return uv;
+}
+
+float2 GetUVBack(float2 uv)
+{
+	uv.y = mUVInversedBack.z + mUVInversedBack.w * uv.y;
+	return uv;
+}
+
+#ifdef _MATERIAL_LIT_
+
+#define lightScale 3.14
+
+float calcD_GGX(float roughness, float dotNH)
+{
+	float alpha = roughness*roughness;
+	float alphaSqr = alpha*alpha;
+	float pi = 3.14159;
+	float denom = dotNH * dotNH *(alphaSqr-1.0) + 1.0;
+	return (alpha / denom) * (alpha / denom) / pi;
+}
+
+float calcF(float F0, float dotLH)
+{
+	float dotLH5 = pow(1.0-dotLH,5.0);
+	return F0 + (1.0-F0)*(dotLH5);
+}
+
+float calcG_Schlick(float roughness, float dotNV, float dotNL)
+{
+	// UE4
+	float k = (roughness + 1.0) * (roughness + 1.0) / 8.0;
+	// float k = roughness * roughness / 2.0;
+
+	float gV = dotNV*(1.0 - k) + k;
+	float gL = dotNL*(1.0 - k) + k;
+
+	return 1.0 / (gV * gL);
+}
+
+float calcLightingGGX(float3 N, float3 V, float3 L, float roughness, float F0)
+{
+	float3 H = normalize(V+L);
+
+	float dotNL = saturate( dot(N,L) );
+	float dotLH = saturate( dot(L,H) );
+	float dotNH = saturate( dot(N,H) ) - 0.001;
+	float dotNV = saturate( dot(N,V) ) + 0.001;
+
+	float D = calcD_GGX(roughness, dotNH);
+	float F = calcF(F0, dotLH);
+	float G = calcG_Schlick(roughness, dotNV, dotNL);
+
+	return dotNL * D * F * G / 4.0;
+}
+
+float3 calcDirectionalLightDiffuseColor(float3 diffuseColor, float3 normal, float3 lightDir, float ao)
+{
+	float3 color = float3(0.0,0.0,0.0);
+
+	float NoL = dot(normal,lightDir);
+	color.xyz = lightColor.xyz * lightScale * max(NoL,0.0) * ao / 3.14;
+	color.xyz = color.xyz * diffuseColor.xyz;
+	return color;
+}
+
+#endif
+
+float4 main( const PS_Input Input ) : SV_Target
+{
+	float2 uv1 = Input.UV1;
+	float2 uv2 = Input.UV2;
+	float3 worldPos = Input.WorldP;
+	float3 worldNormal = Input.WorldN;
+	float3 worldBinormal = Input.WorldB;
+	float3 worldTangent = Input.WorldT;
+
+	float3 pixelNormalDir = worldNormal;
+	float4 vcolor = Input.VColor;
+)";
+
+static char* g_material_ps_suf2_unlit = R"(
+
+
+	float4 Output = float4(emissive, opacity);
+
+	if(opacityMask <= 0.0f) discard;
+	if(opacity <= 0.0) discard;
+
+	return Output;
+}
+
+)";
+
+static char* g_material_ps_suf2_lit = R"(
+	float3 viewDir = normalize(cameraPosition.xyz - worldPos);
+	float3 diffuse = calcDirectionalLightDiffuseColor(baseColor, pixelNormalDir, lightDirection.xyz, ambientOcclusion);
+	float3 specular = lightColor.xyz * lightScale * calcLightingGGX(worldNormal, viewDir, lightDirection.xyz, roughness, 0.9);
+
+	float4 Output =  float4(metallic * specular + (1.0 - metallic) * diffuse, opacity);
+	Output.xyz = Output.xyz + emissive.xyz;
+
+	if(opacityMask <= 0.0) discard;
+	if(opacity <= 0.0) discard;
+
+	return Output;
+}
+
+)";
+
+static char* g_material_ps_suf2_refraction = R"(
+
+	float airRefraction = 1.0;
+	float3 dir = mul((float3x3)cameraMat, pixelNormalDir);
+	dir.y = -dir.y;
+
+	float2 distortUV = 	dir.xy * (refraction - airRefraction);
+
+	distortUV += Input.ScreenUV;
+	distortUV = GetUVBack(distortUV);	
+
+	float4 bg = background_texture.Sample(background_sampler, distortUV);
+	float4 Output = bg;
+
+	if(opacityMask <= 0.0) discard;
+	if(opacity <= 0.0) discard;
+
+	return Output;
+}
+
+)";
