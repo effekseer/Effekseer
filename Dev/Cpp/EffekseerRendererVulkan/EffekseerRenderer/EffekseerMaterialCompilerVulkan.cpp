d00ada557f599e720dc0480c4301b8593161f7db
lriki <lriki.net@gmail.com>
2020-01-26 23:47:13 +0900

    added base code

---
+++ Dev/Cpp/EffekseerRendererVulkan/EffekseerRenderer/EffekseerMaterialCompilerVulkan.cpp
@@ -0,0 +1,147 @@
+#include "EffekseerMaterialCompilerVulkan.h"
+#include <iostream>
+
+#include "../3rdParty/LLGI/src/Vulkan/LLGI.CompilerVulkan.h"
+
+namespace Effekseer
+{
+
+void Serialize(std::vector<uint8_t>& dst, const LLGI::CompilerResult& result)
+{
+
+	uint32_t binarySize = 0;
+
+	binarySize += sizeof(uint32_t);
+
+	for (size_t i = 0; i < result.Binary.size(); i++)
+	{
+		binarySize += sizeof(uint32_t);
+		binarySize += result.Binary[i].size();
+	}
+
+	dst.resize(binarySize);
+
+	uint32_t offset = 0;
+	uint32_t count = result.Binary.size();
+
+	memcpy(dst.data() + offset, &count, sizeof(int32_t));
+	offset += sizeof(int32_t);
+
+	for (size_t i = 0; i < result.Binary.size(); i++)
+	{
+		uint32_t size = result.Binary[i].size();
+
+		memcpy(dst.data() + offset, &size, sizeof(int32_t));
+		offset += sizeof(int32_t);
+
+		memcpy(dst.data() + offset, result.Binary[i].data(), result.Binary[i].size());
+		offset += result.Binary[i].size();
+	}
+}
+
+
+} // namespace Effekseer
+
+namespace Effekseer
+{
+
+class CompiledMaterialBinaryVulkan : public CompiledMaterialBinary, ReferenceObject
+{
+private:
+	std::array<std::vector<uint8_t>, static_cast<int32_t>(MaterialShaderType::Max)> vertexShaders_;
+
+	std::array<std::vector<uint8_t>, static_cast<int32_t>(MaterialShaderType::Max)> pixelShaders_;
+
+public:
+	CompiledMaterialBinaryVulkan() {}
+
+	virtual ~CompiledMaterialBinaryVulkan() {}
+
+	void SetVertexShaderData(MaterialShaderType type, const std::vector<uint8_t>& data)
+	{
+		vertexShaders_.at(static_cast<int>(type)) = data;
+	}
+
+	void SetPixelShaderData(MaterialShaderType type, const std::vector<uint8_t>& data) { pixelShaders_.at(static_cast<int>(type)) = data; }
+
+	const uint8_t* GetVertexShaderData(MaterialShaderType type) const override { return vertexShaders_.at(static_cast<int>(type)).data(); }
+
+	int32_t GetVertexShaderSize(MaterialShaderType type) const override { return vertexShaders_.at(static_cast<int>(type)).size(); }
+
+	const uint8_t* GetPixelShaderData(MaterialShaderType type) const override { return pixelShaders_.at(static_cast<int>(type)).data(); }
+
+	int32_t GetPixelShaderSize(MaterialShaderType type) const override { return pixelShaders_.at(static_cast<int>(type)).size(); }
+
+	int AddRef() override { return ReferenceObject::AddRef(); }
+
+	int Release() override { return ReferenceObject::Release(); }
+
+	int GetRef() override { return ReferenceObject::GetRef(); }
+};
+
+CompiledMaterialBinary* MaterialCompilerVulkan::Compile(Material* material, int32_t maximumTextureCount)
+{
+	// to use options
+	auto compiler = LLGI::CreateSharedPtr(new LLGI::CompilerVulkan());
+
+	auto binary = new CompiledMaterialBinaryVulkan();
+
+	auto convertToVectorVS = [compiler](const std::string& str) -> std::vector<uint8_t> {
+		std::vector<uint8_t> ret;
+
+		LLGI::CompilerResult result;
+		compiler->Compile(result, str.c_str(), LLGI::ShaderStageType::Vertex);
+
+		if (result.Binary.size() > 0)
+		{
+			Serialize(ret, result);
+		}
+		else
+		{
+			std::cout << "VertexShader Compile Error" << std::endl;
+			std::cout << result.Message << std::endl;
+			std::cout << str << std::endl;
+		}
+
+		return ret;
+	};
+
+	auto convertToVectorPS = [compiler](const std::string& str) -> std::vector<uint8_t> {
+		std::vector<uint8_t> ret;
+
+		LLGI::CompilerResult result;
+		compiler->Compile(result, str.c_str(), LLGI::ShaderStageType::Pixel);
+
+		if (result.Binary.size() > 0)
+		{
+			Serialize(ret, result);
+		}
+		else
+		{
+			std::cout << "PixelShader Compile Error" << std::endl;
+			std::cout << result.Message << std::endl;
+			std::cout << str << std::endl;
+		}
+
+		return ret;
+	};
+
+	auto saveBinary = [&material, &binary, &convertToVectorVS, &convertToVectorPS, &maximumTextureCount](MaterialShaderType type) {
+		throw "Not implemented.";
+	};
+
+	if (material->GetHasRefraction())
+	{
+		saveBinary(MaterialShaderType::Refraction);
+		saveBinary(MaterialShaderType::RefractionModel);
+	}
+
+	saveBinary(MaterialShaderType::Standard);
+	saveBinary(MaterialShaderType::Model);
+
+	return binary;
+}
+
+CompiledMaterialBinary* MaterialCompilerVulkan::Compile(Material* material) { return Compile(material, Effekseer::UserTextureSlotMax); }
+
+} // namespace Effekseer
---
+++ Dev/Cpp/EffekseerRendererVulkan/EffekseerRenderer/EffekseerMaterialCompilerVulkan.h
@@ -0,0 +1,29 @@
+
+#pragma once
+
+#include "../../Effekseer/Effekseer/Material/Effekseer.MaterialCompiler.h"
+#include <vector>
+
+namespace Effekseer
+{
+
+class MaterialCompilerVulkan : public MaterialCompiler, ReferenceObject
+{
+private:
+public:
+	MaterialCompilerVulkan() = default;
+
+	virtual ~MaterialCompilerVulkan() = default;
+
+	CompiledMaterialBinary* Compile(Material* material, int32_t maximumTextureCount);
+
+	CompiledMaterialBinary* Compile(Material* material) override;
+
+	int AddRef() override { return ReferenceObject::AddRef(); }
+
+	int Release() override { return ReferenceObject::Release(); }
+
+	int GetRef() override { return ReferenceObject::GetRef(); }
+};
+
+} // namespace Effekseer
