96b37566a0aa9d4e3d17287fa7634d13e7e9ab37
y.omae <y.omae@snk-corp.co.jp>
2020-02-03 10:09:23 +0900

    fix compiled shader add shader compiler for version 16

---
+++ Dev/Cpp/EffekseerRendererDX11/EffekseerRenderer/compile_effect_v16.bat
@@ -0,0 +1,37 @@
+"%DXSDK_DIR%Utilities\\bin\x86\fxc" /Zpc /Tvs_5_0 /EVS /Fh Shader/EffekseerRenderer.Tool_VS.h Shader/tool_renderer_VS.fx
+"%DXSDK_DIR%Utilities\\bin\x86\fxc" /Zpc /Tps_5_0 /EPS /Fh Shader/EffekseerRenderer.Tool_PS.h Shader/tool_renderer_PS.fx
+"%DXSDK_DIR%Utilities\\bin\x86\fxc" /Zpc /Tps_5_0 /EPS /Fh Shader/EffekseerRenderer.ToolNoTexture_PS.h Shader/tool_renderer_no_texture_PS.fx
+
+"%DXSDK_DIR%Utilities\\bin\x86\fxc" /Zpc /Tvs_5_0 /EVS /D __EFFEKSEER_BUILD_VERSION16__=1 /Fh Shader/EffekseerRenderer.Standard_VS.h Shader/standard_renderer_VS.fx
+"%DXSDK_DIR%Utilities\\bin\x86\fxc" /Zpc /Tps_5_0 /EPS /D __EFFEKSEER_BUILD_VERSION16__=1 /Fh Shader/EffekseerRenderer.Standard_PS.h Shader/standard_renderer_PS.fx
+"%DXSDK_DIR%Utilities\\bin\x86\fxc" /Zpc /Tps_5_0 /EPS /Fh Shader/EffekseerRenderer.StandardNoTexture_PS.h Shader/standard_renderer_no_texture_PS.fx
+
+"%DXSDK_DIR%Utilities\\bin\x86\fxc" /Zpc /Tvs_5_0 /EVS /D __EFFEKSEER_BUILD_VERSION16__=1 /Fh Shader/EffekseerRenderer.Standard_Distortion_VS.h Shader/standard_renderer_distortion_VS.fx
+"%DXSDK_DIR%Utilities\\bin\x86\fxc" /Zpc /Tps_5_0 /EPS /D __EFFEKSEER_BUILD_VERSION16__=1 /Fh Shader/EffekseerRenderer.Standard_Distortion_PS.h Shader/standard_renderer_distortion_PS.fx
+"%DXSDK_DIR%Utilities\\bin\x86\fxc" /Zpc /Tps_5_0 /EPS /Fh Shader/EffekseerRenderer.StandardNoTexture_Distortion_PS.h Shader/standard_renderer_no_texture_distortion_PS.fx
+
+"%DXSDK_DIR%Utilities\\bin\x86\fxc" /Zpc /Tvs_5_0 /EVS /D __EFFEKSEER_BUILD_VERSION16__=1 /Fh Shader/EffekseerRenderer.Standard_Lighting_VS.h Shader/standard_renderer_lighting_VS.fx
+"%DXSDK_DIR%Utilities\\bin\x86\fxc" /Zpc /Tps_5_0 /EPS /D __EFFEKSEER_BUILD_VERSION16__=1 /Fh Shader/EffekseerRenderer.Standard_Lighting_PS.h Shader/standard_renderer_lighting_PS.fx
+
+"%DXSDK_DIR%Utilities\\bin\x86\fxc" /Zpc /Tvs_5_0 /EVS /D __EFFEKSEER_BUILD_VERSION16__=1 /Fh Shader/EffekseerRenderer.ModelRenderer.ShaderLightingTexture_VS.h Shader/model_renderer_lighting_texture_VS.fx
+"%DXSDK_DIR%Utilities\\bin\x86\fxc" /Zpc /Tvs_5_0 /EVS /D __EFFEKSEER_BUILD_VERSION16__=1 /Fh Shader/EffekseerRenderer.ModelRenderer.ShaderLighting_VS.h Shader/model_renderer_lighting_VS.fx
+"%DXSDK_DIR%Utilities\\bin\x86\fxc" /Zpc /Tvs_5_0 /EVS /D __EFFEKSEER_BUILD_VERSION16__=1 /Fh Shader/EffekseerRenderer.ModelRenderer.ShaderTexture_VS.h Shader/model_renderer_texture_VS.fx
+"%DXSDK_DIR%Utilities\\bin\x86\fxc" /Zpc /Tvs_5_0 /EVS /D __EFFEKSEER_BUILD_VERSION16__=1 /Fh Shader/EffekseerRenderer.ModelRenderer.Shader_VS.h Shader/model_renderer_VS.fx
+
+"%DXSDK_DIR%Utilities\\bin\x86\fxc" /Zpc /Tvs_5_0 /EVS /D __EFFEKSEER_BUILD_VERSION16__=1 /Fh Shader/EffekseerRenderer.ModelRenderer.ShaderLightingNormal_VS.h Shader/model_renderer_lighting_normal_VS.fx
+"%DXSDK_DIR%Utilities\\bin\x86\fxc" /Zpc /Tvs_5_0 /EVS /D __EFFEKSEER_BUILD_VERSION16__=1 /Fh Shader/EffekseerRenderer.ModelRenderer.ShaderLightingTextureNormal_VS.h Shader/model_renderer_lighting_texture_normal_VS.fx
+
+
+"%DXSDK_DIR%Utilities\\bin\x86\fxc" /Zpc /Tps_5_0 /EPS /D __EFFEKSEER_BUILD_VERSION16__=1 /Fh Shader/EffekseerRenderer.ModelRenderer.ShaderLightingTexture_PS.h Shader/model_renderer_lighting_texture_PS.fx
+"%DXSDK_DIR%Utilities\\bin\x86\fxc" /Zpc /Tps_5_0 /EPS /D __EFFEKSEER_BUILD_VERSION16__=1 /Fh Shader/EffekseerRenderer.ModelRenderer.ShaderLighting_PS.h Shader/model_renderer_lighting_PS.fx
+"%DXSDK_DIR%Utilities\\bin\x86\fxc" /Zpc /Tps_5_0 /EPS /D __EFFEKSEER_BUILD_VERSION16__=1 /Fh Shader/EffekseerRenderer.ModelRenderer.ShaderTexture_PS.h Shader/model_renderer_texture_PS.fx
+"%DXSDK_DIR%Utilities\\bin\x86\fxc" /Zpc /Tps_5_0 /EPS /D __EFFEKSEER_BUILD_VERSION16__=1 /Fh Shader/EffekseerRenderer.ModelRenderer.Shader_PS.h Shader/model_renderer_PS.fx
+
+"%DXSDK_DIR%Utilities\\bin\x86\fxc" /Zpc /Tps_5_0 /EPS /D __EFFEKSEER_BUILD_VERSION16__=1 /Fh Shader/EffekseerRenderer.ModelRenderer.ShaderLightingNormal_PS.h Shader/model_renderer_lighting_normal_PS.fx
+"%DXSDK_DIR%Utilities\\bin\x86\fxc" /Zpc /Tps_5_0 /EPS /D __EFFEKSEER_BUILD_VERSION16__=1 /Fh Shader/EffekseerRenderer.ModelRenderer.ShaderLightingTextureNormal_PS.h Shader/model_renderer_lighting_texture_normal_PS.fx
+
+
+"%DXSDK_DIR%Utilities\\bin\x86\fxc" /Zpc /Tvs_5_0 /EVS /D __EFFEKSEER_BUILD_VERSION16__=1 /Fh Shader/EffekseerRenderer.ModelRenderer.ShaderDistortion_VS.h Shader/model_renderer_distortion_VS.fx
+"%DXSDK_DIR%Utilities\\bin\x86\fxc" /Zpc /Tps_5_0 /EPS /D __EFFEKSEER_BUILD_VERSION16__=1 /Fh Shader/EffekseerRenderer.ModelRenderer.ShaderDistortionTexture_PS.h Shader/model_renderer_distortion_texture_PS.fx
+"%DXSDK_DIR%Utilities\\bin\x86\fxc" /Zpc /Tps_5_0 /EPS /D __EFFEKSEER_BUILD_VERSION16__=1 /Fh Shader/EffekseerRenderer.ModelRenderer.ShaderDistortion_PS.h Shader/model_renderer_distortion_PS.fx
+pause
