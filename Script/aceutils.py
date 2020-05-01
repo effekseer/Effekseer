32640e0dfccca80c7b4e757977bf751abd6226ab
take-cheeze <takechi101010@gmail.com>
2020-05-01 19:37:34 +0900

    Check exit code of command call

--- Script/aceutils.py
+++ Script/aceutils.py
@@ -58,6 +58,8 @@ def call( cmd , env=None):
 	print( cmd )
 	p = subprocess.Popen(cmd, shell=True, env=env)
 	ret = p.wait()
+	if ret != 0:
+		raise RuntimeError('failed command: "{}" with {}'.format(cmd, ret))
 	print('')
 
 
