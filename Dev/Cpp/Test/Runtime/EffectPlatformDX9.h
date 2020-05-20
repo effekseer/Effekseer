7606b74f41726425c7f3b3a5f36afdbf0b478f88
durswd <swda.durl@gmail.com>
2020-05-21 00:37:24 +0900

    Fix DirectX9 test

--- Dev/Cpp/Test/Runtime/EffectPlatformDX9.h
+++ Dev/Cpp/Test/Runtime/EffectPlatformDX9.h
@@ -3,6 +3,26 @@
 #include "../../EffekseerRendererDX9/EffekseerRendererDX9.h"
 #include "EffectPlatformGLFW.h"
 
+class DistortingCallbackDX9 : public EffekseerRenderer::DistortingCallback
+{
+	::EffekseerRendererDX9::Renderer* renderer = nullptr;
+	LPDIRECT3DDEVICE9 device = nullptr;
+	LPDIRECT3DTEXTURE9 texture = nullptr;
+	int32_t texWidth_ = 0;
+	int32_t texHeight_ = 0;
+
+public:
+	DistortingCallbackDX9(::EffekseerRendererDX9::Renderer* renderer, LPDIRECT3DDEVICE9 device, int texWidth, int texHeight);
+
+	virtual ~DistortingCallbackDX9();
+
+	virtual bool OnDistorting() override;
+
+	void Lost();
+
+	void Reset();
+};
+
 class EffectPlatformDX9 final : public EffectPlatformGLFW
 {
 private:
@@ -11,6 +31,7 @@ private:
 	bool vsync_ = false;
 	bool fullscreen_ = false;
 	IDirect3DSurface9* checkedSurface_ = nullptr;
+	DistortingCallbackDX9* distorting_ = nullptr;
 
 	void CreateCheckedSurface();
 
