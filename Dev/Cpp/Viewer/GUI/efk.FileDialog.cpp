47e61f672e663b5d6f023eecdaaa13eb694cbffe
durswd <swda.durl@gmail.com>
2020-07-21 21:57:17 +0900

    Remove unused function

--- Dev/Cpp/Viewer/GUI/efk.FileDialog.cpp
+++ Dev/Cpp/Viewer/GUI/efk.FileDialog.cpp
@@ -39,59 +39,6 @@ namespace efk
 
 std::u16string FileDialog::temp = u"";
 
-int32_t ConvertUtf8ToUtf16_(char16_t* dst, int32_t dst_size, const char* src)
-{
-    int32_t i, code = 0;
-    int8_t c0, c1, c2 = 0;
-    int8_t* srci = reinterpret_cast<int8_t*>(const_cast<char*>(src));
-    if (dst_size == 0)
-        return 0;
-
-    dst_size -= 1;
-
-    for (i = 0; i < dst_size; i++)
-    {
-        uint16_t wc;
-
-        c0 = *srci;
-        srci++;
-        if (c0 == '\0')
-        {
-            break;
-        }
-        // UTF8からUTF16に変換
-        code = (uint8_t)c0 >> 4;
-        if (code <= 7)
-        {
-            // 8bit文字
-            wc = c0;
-        }
-        else if (code >= 12 && code <= 13)
-        {
-            // 16bit文字
-            c1 = *srci;
-            srci++;
-            wc = ((c0 & 0x1F) << 6) | (c1 & 0x3F);
-        }
-        else if (code == 14)
-        {
-            // 24bit文字
-            c1 = *srci;
-            srci++;
-            c2 = *srci;
-            srci++;
-            wc = ((c0 & 0x0F) << 12) | ((c1 & 0x3F) << 6) | (c2 & 0x3F);
-        }
-        else
-        {
-            continue;
-        }
-        dst[i] = wc;
-    }
-    dst[i] = 0;
-    return i;
-}
-
 const char16_t* FileDialog::OpenDialog(const char16_t* filterList, const char16_t* defaultPath)
 {
 	char filterList_[256], defaultPath_[1024];
@@ -104,12 +51,12 @@ const char16_t* FileDialog::OpenDialog(const char16_t* filterList, const char16_
 	if (result == NFD_OKAY)
 	{
 		std::array<char16_t, 1024> outPath_;
-        outPath_.fill(0);
+		outPath_.fill(0);
 
 		Effekseer::ConvertUtf8ToUtf16(outPath_.data(), 1024, outPath);
 
 		temp = std::u16string(outPath_.data());
-        
+
 		free(outPath);
 		return temp.c_str();
 	}
