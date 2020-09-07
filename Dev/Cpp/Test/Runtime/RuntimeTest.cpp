8c2f7cf5e45fffeaffd02e4dbf39a84426229ba7
durswd <swda.durl@gmail.com>
2020-09-07 13:52:18 +0900

    Add test code

--- Dev/Cpp/Test/Runtime/RuntimeTest.cpp
+++ Dev/Cpp/Test/Runtime/RuntimeTest.cpp
@@ -79,6 +79,7 @@ void BasicRuntimeTestPlatform(EffectPlatform* platform, std::string baseResultPa
 	single15Test(u"Material_Refraction", "Material_Refraction");
 	single15Test(u"Material_WorldPositionOffset", "Material_WorldPositionOffset");
 	single15Test(u"BasicRenderSettings_Blend", "BasicRenderSettings_Blend");
+	single15Test(u"Material_FresnelRotatorPolarCoords", "Material_FresnelRotatorPolarCoords");
 }
 
 void BasicRuntimeDeviceLostTest()
