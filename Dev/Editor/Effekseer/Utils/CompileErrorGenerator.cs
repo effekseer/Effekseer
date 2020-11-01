3cd269913e9f36265f38c90375d27ee75154e57a
NumAniCloud <num.ani.cloud@gmail.com>
2020-11-02 00:24:50 +0900

    replace casts by pattern matching in CompileErrorGenerator.

--- Dev/Editor/Effekseer/Utils/CompileErrorGenerator.cs
+++ Dev/Editor/Effekseer/Utils/CompileErrorGenerator.cs
@@ -1,17 +1,16 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using Effekseer.InternalScript;
+using InvalidOperationException = Effekseer.InternalScript.InvalidOperationException;
 
 namespace Effekseer.Utils
 {
-	class CompileErrorGenerator
+	internal static class CompileErrorGenerator
 	{
 		public static string Generate(string code, Exception e)
 		{
 			List<int> separators = new List<int>();
-			for(int i = 0; i < code.Length; i++)
+			for (int i = 0; i < code.Length; i++)
 			{
 				if (code[i] == '\n')
 				{
@@ -22,70 +21,50 @@ namespace Effekseer.Utils
 			int errorLine = 0;
 			int errorPos = 0;
 
-			if(e is InternalScript.CompileException)
+			if (e is CompileException compileException)
 			{
-				var e_ = e as InternalScript.CompileException;
-				GetErrorPosition(separators, e_.Line, out errorLine, out errorPos);
+				GetErrorPosition(separators, compileException.Line, out errorLine, out errorPos);
 			}
 
 			var errorLinePos = string.Format(Resources.GetString("CompilePosition"), errorLine, errorPos);
 
-			if(e is InternalScript.InvalidTokenException)
-			{
-				var e_ = e as InternalScript.InvalidTokenException;
 
-				return string.Format(Resources.GetString("CompileErrror_InvalidToken"), e_.Message) + errorLinePos;
-			}
-			else if (e is InternalScript.InvalidEOFException)
+			switch (e)
 			{
-				var e_ = e as InternalScript.InvalidEOFException;
+			case InvalidTokenException exception:
+				return string.Format(Resources.GetString("CompileErrror_InvalidToken"), exception.Message) + errorLinePos;
 
+			case InvalidEOFException _:
 				return string.Format(Resources.GetString("CompileErrror_InvalidEoF")) + errorLinePos;
-			}
-			else if (e is InternalScript.InvalidOperationException)
-			{
-				var e_ = e as InternalScript.InvalidOperationException;
 
+			case InvalidOperationException _:
 				return string.Format(Resources.GetString("CompileError_InvalidOperation")) + errorLinePos;
-			}
-			else if (e is InternalScript.ArgSizeException)
-			{
-				var e_ = e as InternalScript.ArgSizeException;
 
-				return string.Format(Resources.GetString("CompileError_ArgSize"), string.Join(",", e_.Expected), e_.Actual) + errorLinePos;
-			}
-			else if (e is InternalScript.InvalidSubstitution)
-			{
-				var e_ = e as InternalScript.InvalidSubstitution;
+			case ArgSizeException exception:
+				return string.Format(Resources.GetString("CompileError_ArgSize"), string.Join(",", exception.Expected), exception.Actual) + errorLinePos;
 
+			case InvalidSubstitution _:
 				return string.Format(Resources.GetString("CompileError_InvalidSubstitution")) + errorLinePos;
-			}
-			else if (e is InternalScript.UnknownFunctionException)
-			{
-				var e_ = e as InternalScript.UnknownFunctionException;
 
-				return string.Format(Resources.GetString("CompileError_UnknownFunction"), e_.Message) + errorLinePos;
-			}
-			else if (e is InternalScript.UnknownValueException)
-			{
-				var e_ = e as InternalScript.UnknownValueException;
+			case UnknownFunctionException exception:
+				return string.Format(Resources.GetString("CompileError_UnknownFunction"), exception.Message) + errorLinePos;
 
-				return string.Format(Resources.GetString("CompileError_UnknownValue"), e_.Message) + errorLinePos;
-			}
-			else
-			{
+			case UnknownValueException exception:
+				return string.Format(Resources.GetString("CompileError_UnknownValue"), exception.Message) + errorLinePos;
+
+			default:
 				return "Unknown error";
 			}
 		}
 
-		static void GetErrorPosition(List<int> sep, int ind, out int line, out int pos)
+		private static void GetErrorPosition(List<int> sep, int ind, out int line, out int pos)
 		{
 			line = 0;
 			pos = 0;
 
-			for(int i = 0; i < sep.Count; i++)
+			for (int i = 0; i < sep.Count; i++)
 			{
-				if(ind < sep[i])
+				if (ind < sep[i])
 				{
 					line = i + 1;
 
