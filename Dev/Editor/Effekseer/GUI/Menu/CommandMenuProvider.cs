e537544f8aa1e33b8c389c4a5d0b1b13b439e7a7
NumAniCloud <num.ani.cloud@gmail.com>
2020-11-02 11:06:59 +0900

    Replace repeatedly invocation with array, in CommandMenuProvider.
    Extract methods

--- Dev/Editor/Effekseer/GUI/Menu/CommandMenuProvider.cs
+++ Dev/Editor/Effekseer/GUI/Menu/CommandMenuProvider.cs
@@ -1,6 +1,6 @@
 ﻿using System;
 using System.Linq;
-using Effekseer.Utils;
+using Effekseer.Script;
 
 namespace Effekseer.GUI.Menu
 {
@@ -58,106 +58,33 @@ namespace Effekseer.GUI.Menu
 			return menu;
 		}
 
-		private static void AddRange(Menu menu, IControl[] controls)
-		{
-			foreach (IControl control in controls)
-			{
-				menu.Controls.Add(control);
-			}
-		}
-
 		public static Menu SetupFilesMenu(RecentFilesMenuManager recentFiles)
 		{
-			var file = new MultiLanguageString("Files");
-			var input = new MultiLanguageString("Import");
-			var output = new MultiLanguageString("Export");
-
-			var menu = new Menu(file);
-			menu.Controls.Add(CreateMenuItemFromCommands(Commands.New));
-			menu.Controls.Add(CreateMenuItemFromCommands(Commands.Open));
-			menu.Controls.Add(CreateMenuItemFromCommands(Commands.Overwrite));
-			menu.Controls.Add(CreateMenuItemFromCommands(Commands.SaveAs));
-
-			menu.Controls.Add(new MenuSeparator());
-
-			menu.Controls.Add(SetupImportSubMenu(input));
-
-			menu.Controls.Add(SetupExportSubMenu(output));
-
-			menu.Controls.Add(new MenuSeparator());
-
+			var menu = new Menu(new MultiLanguageString("Files"));
+			
+			recentFiles.Reload();
+			AddRange(menu, new IControl[]
 			{
-				recentFiles.Reload();
-				menu.Controls.Add(recentFiles.Menu);
-			}
+				CreateMenuItemFromCommands(Commands.New),
+				CreateMenuItemFromCommands(Commands.Open),
+				CreateMenuItemFromCommands(Commands.Overwrite),
+				CreateMenuItemFromCommands(Commands.SaveAs),
 
-			menu.Controls.Add(new MenuSeparator());
+				new MenuSeparator(),
 
-			menu.Controls.Add(CreateMenuItemFromCommands(Commands.Exit));
+				SetupImportSubMenu(new MultiLanguageString("Import")),
+				SetupExportSubMenu(new MultiLanguageString("Export")),
+
+				new MenuSeparator(),
+
+				recentFiles.Menu,
+
+				new MenuSeparator(),
+
+				CreateMenuItemFromCommands(Commands.Exit),
+			});
 
 			return menu;
-		}
-		
-
-		private static Menu SetupExportSubMenu(MultiLanguageString output)
-		{
-			var export_menu = new Menu(output, Icons.Empty);
-
-			for (int c = 0; c < Core.ExportScripts.Count; c++)
-			{
-				var item = new MenuItem();
-				var script = Core.ExportScripts[c];
-				item.Label = script.Title;
-				item.Clicked += () =>
-				{
-					var filter = script.Filter.Split('.').Last();
-					var result = swig.FileDialog.SaveDialog(filter, System.IO.Directory.GetCurrentDirectory());
-
-					if (!string.IsNullOrEmpty(result))
-					{
-						if (System.IO.Path.GetExtension(result) != "." + filter)
-						{
-							result += "." + filter;
-						}
-
-						var filepath = result;
-						script.Function(filepath);
-
-						System.IO.Directory.SetCurrentDirectory(System.IO.Path.GetDirectoryName(filepath));
-					}
-				};
-				export_menu.Controls.Add(item);
-			}
-
-			return export_menu;
-		}
-
-		private static Menu SetupImportSubMenu(MultiLanguageString input)
-		{
-			var import_menu = new Menu(input, Icons.Empty);
-
-			for (int c = 0; c < Core.ImportScripts.Count; c++)
-			{
-				var item = new MenuItem();
-				var script = Core.ImportScripts[c];
-				item.Label = script.Title;
-				item.Clicked += () =>
-				{
-					var filter = script.Filter.Split('.').Last();
-					var result = swig.FileDialog.OpenDialog(filter, System.IO.Directory.GetCurrentDirectory());
-
-					if (!string.IsNullOrEmpty(result))
-					{
-						var filepath = result;
-
-						script.Function(filepath);
-						System.IO.Directory.SetCurrentDirectory(System.IO.Path.GetDirectoryName(filepath));
-					}
-				};
-				import_menu.Controls.Add(item);
-			}
-
-			return import_menu;
 		}
 
 		private static MenuItem CreateMenuItemFromCommands(Func<bool> onClicked)
@@ -170,6 +97,84 @@ namespace Effekseer.GUI.Menu
 			item.Clicked += () => { onClicked(); };
 
 			return item;
+		}
+
+		private static void AddRange(Menu menu, IControl[] controls)
+		{
+			foreach (IControl control in controls)
+			{
+				menu.Controls.Add(control);
+			}
+		}
+
+		private static Menu SetupImportSubMenu(MultiLanguageString input)
+		{
+			var importMenu = new Menu(input, Icons.Empty);
+
+			for (int c = 0; c < Core.ImportScripts.Count; c++)
+			{
+				var item = new MenuItem();
+				var script = Core.ImportScripts[c];
+				item.Label = script.Title;
+				item.Clicked += () => Import(script);
+				importMenu.Controls.Add(item);
+			}
+
+			return importMenu;
+		}
+
+		private static void Import(ImportScript script)
+		{
+			var filter = script.Filter.Split('.').Last();
+			var result = swig.FileDialog.OpenDialog(filter, System.IO.Directory.GetCurrentDirectory());
+
+			if (string.IsNullOrEmpty(result))
+			{
+				return;
+			}
+
+			var filepath = result;
+
+			script.Function(filepath);
+			System.IO.Directory.SetCurrentDirectory(System.IO.Path.GetDirectoryName(filepath));
+		}
+		
+
+		private static Menu SetupExportSubMenu(MultiLanguageString output)
+		{
+			var exportMenu = new Menu(output, Icons.Empty);
+
+			for (int c = 0; c < Core.ExportScripts.Count; c++)
+			{
+				var item = new MenuItem();
+				var script = Core.ExportScripts[c];
+				item.Label = script.Title;
+				item.Clicked += () => Export(script);
+				exportMenu.Controls.Add(item);
+			}
+
+			return exportMenu;
+		}
+
+		private static void Export(ExportScript script)
+		{
+			var filter = script.Filter.Split('.').Last();
+			var result = swig.FileDialog.SaveDialog(filter, System.IO.Directory.GetCurrentDirectory());
+
+			if (string.IsNullOrEmpty(result))
+			{
+				return;
+			}
+
+			if (System.IO.Path.GetExtension(result) != "." + filter)
+			{
+				result += "." + filter;
+			}
+
+			var filepath = result;
+			script.Function(filepath);
+
+			System.IO.Directory.SetCurrentDirectory(System.IO.Path.GetDirectoryName(filepath));
 		}
 	}
 }
