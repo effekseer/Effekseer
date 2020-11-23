1d74d39989f1ef70b74b96b5773ede86148bcf34
ueshita <nalto32@gmail.com>
2020-11-23 22:42:13 +0900

    Change SoundAL loader to RefPtr.

--- Dev/Cpp/EffekseerSoundAL/EffekseerSound/EffekseerSoundAL.SoundImplemented.h
+++ Dev/Cpp/EffekseerSoundAL/EffekseerSound/EffekseerSoundAL.SoundImplemented.h
@@ -41,7 +41,7 @@ public:
 	
 	::Effekseer::SoundPlayer* CreateSoundPlayer();
 
-	::Effekseer::SoundLoader* CreateSoundLoader( ::Effekseer::FileInterface* fileInterface = NULL );
+	::Effekseer::SoundLoaderRef CreateSoundLoader( ::Effekseer::FileInterface* fileInterface = NULL );
 	
 	void StopAllVoices();
 
