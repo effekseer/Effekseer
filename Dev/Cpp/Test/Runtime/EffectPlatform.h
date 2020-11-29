0ab8c52e7aed0e5b077397baec818932e5a37cbc
durswd <swda.durl@gmail.com>
2020-11-30 01:17:07 +0900

    Implementing softparticle test

--- Dev/Cpp/Test/Runtime/EffectPlatform.h
+++ Dev/Cpp/Test/Runtime/EffectPlatform.h
@@ -38,7 +38,6 @@ protected:
 	std::vector<std::vector<uint8_t>> buffers_;
 	std::vector<uint32_t> checkeredPattern_;
 
-	EffekseerRenderer::RendererRef GetRenderer() const;
 	virtual void* GetNativePtr(int32_t index)
 	{
 		return nullptr;
@@ -100,6 +99,40 @@ public:
 	Effekseer::ManagerRef GetManager() const
 	{
 		return manager_;
+	}
+
+	EffekseerRenderer::RendererRef GetRenderer() const;
+
+	void GenerateDepth()
+	{
+		auto projMat = renderer_->GetProjectionMatrix();
+		auto cameraMat = renderer_->GetCameraMatrix();
+
+		Effekseer::Vector3D pos{0.0f, 0.0f, 0.0f};
+
+		Effekseer::Vector3D::TransformWithW(pos, pos, cameraMat);
+		Effekseer::Vector3D::TransformWithW(pos, pos, projMat);
+
+		std::array<float, 4> posArray;
+		posArray.fill(1.0f);
+		posArray[0] = pos.Z;
+
+		{
+			Effekseer::Backend::TextureParameter texParam;
+			texParam.InitialData.resize(sizeof(float) * 4);
+			memcpy(texParam.InitialData.data(), posArray.data(), texParam.InitialData.size());
+
+			auto depth = GetRenderer()->GetGraphicsDevice()->CreateTexture(texParam);
+
+			EffekseerRenderer::DepthReconstructionParameter reconstructionParam;
+			reconstructionParam.DepthBufferScale = 1.0f;
+			reconstructionParam.DepthBufferOffset = 0.0f;
+			reconstructionParam.ProjectionMatrix33 = projMat.Values[2][2];
+			reconstructionParam.ProjectionMatrix43 = projMat.Values[2][3];
+			reconstructionParam.ProjectionMatrix34 = projMat.Values[3][2];
+			reconstructionParam.ProjectionMatrix44 = projMat.Values[3][3];
+			GetRenderer()->SetDepth(depth, reconstructionParam);
+		}
 	}
 
 	const std::vector<Effekseer::EffectRef>& GetEffects() const
