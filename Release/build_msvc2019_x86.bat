35ffe978e741baa6a22663b7d6be476258d53f6c
durswd <swda.durl@gmail.com>
2020-12-07 21:15:28 +0900

    Update build runtime scripts

---
+++ Release/build_msvc2019_x86.bat
@@ -0,0 +1,5 @@
+mkdir build_msvc2019
+cd build_msvc2019
+cmake -G "Visual Studio 16 2019" -A Win32 -DCMAKE_INSTALL_PREFIX=../install_msvc2019 -DUSE_LLGI=ON -DBUILD_DX12=ON .. -DCMAKE_DEBUG_POSTFIX=d
+cmake --build . --config Debug --target INSTALL
+cmake --build . --config Release --target INSTALL
