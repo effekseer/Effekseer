ef12e35f376533ab548a418fd3f7fbea0a6f2f2d
durswd <swda.durl@gmail.com>
2021-02-08 19:46:43 +0900

    Fix about a sanitizer on msvc

--- Script/GenerateProjects_Sanitizer_MSVC.bat
+++ Script/GenerateProjects_Sanitizer_MSVC.bat
@@ -5,7 +5,7 @@ mkdir ..\build_sanitizer
 
 cd /d ..\build_sanitizer
 
-cmake -A Win32 -D BUILD_VIEWER=ON -D BUILD_EDITOR=ON -D BUILD_TEST=ON -D BUILD_VULKAN=ON ../
+cmake -A Win32 -D BUILD_VIEWER=ON -D BUILD_EDITOR=ON -D BUILD_TEST=ON ../
 
 cd /d %~dp0
 
