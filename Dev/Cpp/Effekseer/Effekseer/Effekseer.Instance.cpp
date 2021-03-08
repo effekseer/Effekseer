52745979e3bfc9225dc2f2f1f1ad90404c7c4b36
durswd <swda.durl@gmail.com>
2021-03-09 00:49:17 +0900

    Revert "Fix a bug where uv animation is sometimes flipped"
    This reverts commit 5c5989b7391c40e423757e70d9c330f48ebd189c.

--- Dev/Cpp/Effekseer/Effekseer/Effekseer.Instance.cpp
+++ Dev/Cpp/Effekseer/Effekseer/Effekseer.Instance.cpp
@@ -1797,14 +1797,7 @@ RectF Instance::GetUV() const
 	}
 	else if (m_pEffectNode->RendererCommon.UVType == ParameterRendererCommon::UV_ANIMATION)
 	{
-		auto offset = uvTimeOffset;
-		if (offset > std::numeric_limits<int32_t>::max() / 1000)
-		{
-			const auto allFrameLength = m_pEffectNode->RendererCommon.UV.Animation.FrameCountX * m_pEffectNode->RendererCommon.UV.Animation.FrameCountY * m_pEffectNode->RendererCommon.UV.Animation.FrameLength;
-			offset -= allFrameLength * (std::numeric_limits<int32_t>::max() / 1000 / allFrameLength);
-		}
-
-		auto time = m_LivingTime + offset;
+		auto time = m_LivingTime + uvTimeOffset;
 
 		int32_t frameNum = (int32_t)(time / m_pEffectNode->RendererCommon.UV.Animation.FrameLength);
 		int32_t frameCount = m_pEffectNode->RendererCommon.UV.Animation.FrameCountX * m_pEffectNode->RendererCommon.UV.Animation.FrameCountY;
