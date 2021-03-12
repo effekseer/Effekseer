4249ab77955aca2ec97976ed40855e22ede99376
durswd <swda.durl@gmail.com>
2021-03-12 17:55:54 +0900

    Revert "Revert to version to 1.53b"
    This reverts commit d47f95212a407f123bd794c0f40f1de3b5eaab00.

--- Dev/Editor/EffekseerCore/Core.cs
+++ Dev/Editor/EffekseerCore/Core.cs
@@ -146,7 +146,7 @@ namespace Effekseer
 
 	public class Core
 	{
-		public const string Version = "1.53b";
+		public const string Version = "1.53c";
 
 		public const string OptionFilePath = "config.option.xml";
 
