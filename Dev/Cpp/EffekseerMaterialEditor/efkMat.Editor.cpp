98a5e07834637e610d3c24f2c068b2ba738f9ae9
durswd <swda.durl@gmail.com>
2021-03-27 00:15:02 +0900

    Add a feature to show maximum parameter count

--- Dev/Cpp/EffekseerMaterialEditor/efkMat.Editor.cpp
+++ Dev/Cpp/EffekseerMaterialEditor/efkMat.Editor.cpp
@@ -728,6 +728,7 @@ void Editor::UpdateNodes()
 
 			preview_->CompileShader(vs, ps, textures, uniforms);
 			previewTextureCount_ = textures.size();
+			previewUniformCount_ = uniforms.size();
 		}
 
 		for (auto node : material->GetNodes())
@@ -744,6 +745,7 @@ void Editor::UpdateNodes()
 
 			preview_->UpdateUniforms(textures, uniforms);
 			previewTextureCount_ = textures.size();
+			previewUniformCount_ = uniforms.size();
 		}
 	}
 
@@ -1545,6 +1547,22 @@ void Editor::UpdatePreview()
 	else
 	{
 		ImGui::Text((textureNumHeader + " %d / %d").c_str(), previewTextureCount_, Effekseer::UserTextureSlotMax);
+	}
+
+	// uniform
+	
+	// TODO : refactor
+	auto uniformNumHeader = StringContainer::GetValue("UniformCount", "Uniform");
+	const int uniformMax = 16;
+
+	if (previewUniformCount_ > uniformMax)
+	{
+		ImGui::TextColored(
+			ImColor(255, 0, 0, 255), (uniformNumHeader + " %d / %d").c_str(), previewUniformCount_, uniformMax);
+	}
+	else
+	{
+		ImGui::Text((uniformNumHeader + " %d / %d").c_str(), previewUniformCount_, uniformMax);
 	}
 }
 
