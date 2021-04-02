ed8f935aba327622eb46ed7a0a09ff91c4caf2df
lriki <lriki.net@gmail.com>
2021-04-02 18:45:58 +0900

    Added Playback controls
    (cherry picked from commit 8dcac96d603e07701457219a108896b9d94d44b3)

--- Dev/Editor/EffekseerLit/GUI/Dock/EffectViwerDock.cs
+++ Dev/Editor/EffekseerLit/GUI/Dock/EffectViwerDock.cs
@@ -15,7 +15,68 @@ namespace Effekseer.GUI.Dock
 
 		protected override void UpdateInternal()
 		{
-			base.UpdateInternal();
+			float textHeight = Manager.NativeManager.GetTextLineHeight();
+			float frameHeight = Manager.NativeManager.GetFrameHeightWithSpacing();
+			float dpiScale = Manager.DpiScale;
+			float padding = 4.0f * dpiScale;
+
+			IsHovered = false;
+
+			var contentSize = Manager.NativeManager.GetContentRegionAvail();
+
+			// Menu
+			contentSize.X = System.Math.Max(1, contentSize.X);
+			contentSize.Y = System.Math.Max(1, contentSize.Y - frameHeight - padding);
+
+			var p = Manager.Native.RenderView((int)contentSize.X, (int)contentSize.Y);
+			Manager.NativeManager.Image(p, (int)contentSize.X, (int)contentSize.Y);
+
+			if (Manager.NativeManager.BeginDragDropTarget())
+			{
+				int size = 0;
+				byte[] data = new byte[5];
+				if (Manager.NativeManager.AcceptDragDropPayload("PayloadName", data, data.Length, ref size))
+				{
+					Console.WriteLine("aaa");
+					//var sourceID = BitConverter.ToInt32(treeView.treePyload, 0);
+					//treeView.exchangeEvents.Add(
+					//	Tuple.Create(sourceID, UniqueID, MovingNodeEventType.AddAsChild));
+				}
+
+				Manager.NativeManager.EndDragDropTarget();
+			}
+
+			IsHovered = Manager.NativeManager.IsWindowHovered();
+
+			Manager.NativeManager.Indent(padding);
+
+			// Enum
+			Manager.NativeManager.PushItemWidth(textHeight * 7.0f);
+			renderMode.Update();
+			Manager.NativeManager.PopItemWidth();
+
+			Manager.NativeManager.SameLine();
+
+			Manager.NativeManager.PushItemWidth(textHeight * 2.5f);
+			viewMode.Update();
+			Manager.NativeManager.PopItemWidth();
+
+			string perfText =
+				"D:" + Manager.Native.GetAndResetDrawCall().ToString("D3") + "  " +
+				"V:" + Manager.Native.GetAndResetVertexCount().ToString("D5") + "  " +
+				"P:" + Manager.Native.GetInstanceCount().ToString("D5") + " ";
+
+			Manager.NativeManager.SameLine(contentSize.X - Manager.NativeManager.CalcTextSize(perfText).X);
+
+			// Display performance information
+			Manager.NativeManager.Text(perfText);
+			if (Manager.NativeManager.IsItemHovered())
+			{
+				Manager.NativeManager.SetTooltip(
+					"D: Draw calls of current rendering.\n" +
+					"V: Vertex count of current rendering.\n" +
+					"P: Particle count of current rendering.");
+			}
 		}
 	}
 }
