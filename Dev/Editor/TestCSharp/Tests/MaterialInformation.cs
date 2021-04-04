049f315da2e0c210992114b5cc82cacb47e584d8
durswd <swda.durl@gmail.com>
2021-04-04 20:45:42 +0900

    Update a test

--- Dev/Editor/TestCSharp/Tests/MaterialInformation.cs
+++ Dev/Editor/TestCSharp/Tests/MaterialInformation.cs
@@ -12,23 +12,9 @@ namespace TestCSharp
 	{
 		public static void Test()
 		{
-			TestMaterialFile("../../TestData/Effects/Materials/CustomData1.efkmat");
-			TestMaterialFile("../../TestData/Effects/Materials/Fresnel.efkmat");
-			TestMaterialFile("../../TestData/Effects/Materials/FresnelLike.efkmat");
-			TestMaterialFile("../../TestData/Effects/Materials/Img6.efkmat");
-			TestMaterialFile("../../TestData/Effects/Materials/Lighting1.efkmat");
-			TestMaterialFile("../../TestData/Effects/Materials/Math1.efkmat");
-			TestMaterialFile("../../TestData/Effects/Materials/Normal.efkmat");
-			TestMaterialFile("../../TestData/Effects/Materials/Parameter23.efkmat");
-			TestMaterialFile("../../TestData/Effects/Materials/PolarCoords.efkmat");
-			TestMaterialFile("../../TestData/Effects/Materials/Refraction.efkmat");
-			TestMaterialFile("../../TestData/Effects/Materials/Refraction_Lighting.efkmat");
-			TestMaterialFile("../../TestData/Effects/Materials/Rotar.efkmat");
-			TestMaterialFile("../../TestData/Effects/Materials/Sampler1.efkmat");
-			TestMaterialFile("../../TestData/Effects/Materials/UV1.efkmat");
-			TestMaterialFile("../../TestData/Effects/Materials/UV2.efkmat");
-			TestMaterialFile("../../TestData/Effects/Materials/VertexColor.efkmat");
-			TestMaterialFile("../../TestData/Effects/Materials/WorldPositionOffset.efkmat");
+			TestMaterialFile("../../TestData/Effects/Materials_Serialized/CustomData1.efkmat");
+			TestMaterialFile("../../TestData/Effects/Materials_Serialized/DepthFade.efkmat");
+			TestMaterialFile("../../TestData/Effects/Materials_Serialized/Parameter23.efkmat");
 		}
 
 		static void TestMaterialFile(string path)
