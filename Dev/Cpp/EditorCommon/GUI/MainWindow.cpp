d96cece3174435381770841a1231177df1119435
durswd <swda.durl@gmail.com>
2021-05-14 19:39:49 +0900

    Fix a bug where MaterialEditor crashes in some Windows environment

--- Dev/Cpp/EditorCommon/GUI/MainWindow.cpp
+++ Dev/Cpp/EditorCommon/GUI/MainWindow.cpp
@@ -31,10 +31,12 @@ bool MainWindow::InitializeInternal(const char16_t* title, MainWindowState state
 
 	if (isOpenGLMode)
 	{
+#if defined(__APPLE__) || defined(__linux__)
 		glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
 		glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 2);
 		glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
 		glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+#endif
 		glfwWindowHint(GLFW_CLIENT_API, GLFW_OPENGL_API);
 	}
 	else
