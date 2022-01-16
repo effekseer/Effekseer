e66b265b3a13221e6c50c60718e1ce77c6214470
durswd <swda.durl@gmail.com>
2022-01-16 23:36:55 +0900

    fix

--- Dev/Cpp/Test/Backend/GpuParticleShaders/HLSL/perticle-render.vert.hlsl
+++ Dev/Cpp/Test/Backend/GpuParticleShaders/HLSL/perticle-render.vert.hlsl
@@ -1,97 +1,65 @@
 //#line 1 "perticle-render.vert.hlsl"
-//#line 1 "./noise.hlsli"
-
+//#line 1 "./noise2d.hlsli"
 
 float3 mod289(float3 x) {
   return x - floor(x * (1.0 / 289.0)) * 289.0;
 }
 
-float4 mod289(float4 x) {
+float2 mod289(float2 x) {
   return x - floor(x * (1.0 / 289.0)) * 289.0;
 }
 
-float4 permute(float4 x) {
-     return mod289(((x*34.0)+1.0)*x);
+float3 permute(float3 x) {
+  return mod289(((x*34.0)+1.0)*x);
 }
 
-float4 taylorInvSqrt(float4 r)
-{
-  return 1.79284291400159 - 0.85373472095314 * r;
-}
-
-float snoise(float3 v)
+float snoise(float2 v)
   {
-  const float2 C = float2(1.0/6.0, 1.0/3.0);
-  const float4 D = float4(0.0, 0.5, 1.0, 2.0);
+  const float4 C = float4(0.211324865405187,  // (3.0-sqrt(3.0))/6.0
+                      0.366025403784439,  // 0.5*(sqrt(3.0)-1.0)
+                     -0.577350269189626,  // -1.0 + 2.0 * C.x
+                      0.024390243902439); // 1.0 / 41.0
+// First corner
+  float2 i  = floor(v + dot(v, C.yy) );
+  float2 x0 = v -   i + dot(i, C.xx);
 
+// Other corners
+  float2 i1;
+  //i1.x = step( x0.y, x0.x ); // x0.x > x0.y ? 1.0 : 0.0
+  //i1.y = 1.0 - i1.x;
+  i1 = (x0.x > x0.y) ? float2(1.0, 0.0) : float2(0.0, 1.0);
+  // x0 = x0 - 0.0 + 0.0 * C.xx ;
+  // x1 = x0 - i1 + 1.0 * C.xx ;
+  // x2 = x0 - 1.0 + 2.0 * C.xx ;
+  float4 x12 = x0.xyxy + C.xxzz;
+  x12.xy -= i1;
 
-  float3 i = floor(v + dot(v, C.yyy) );
-  float3 x0 = v - i + dot(i, C.xxx) ;
+// Permutations
+  i = mod289(i); // Avoid truncation effects in permutation
+  float3 p = permute( permute( i.y + float3(0.0, i1.y, 1.0 ))
+		+ i.x + float3(0.0, i1.x, 1.0 ));
 
+  float3 m = max(0.5 - float3(dot(x0,x0), dot(x12.xy,x12.xy), dot(x12.zw,x12.zw)), 0.0);
+  m = m*m ;
+  m = m*m ;
 
-  float3 g = step(x0.yzx, x0.xyz);
-  float3 l = 1.0 - g;
-  float3 i1 = min( g.xyz, l.zxy );
-  float3 i2 = max( g.xyz, l.zxy );
+// Gradients: 41 points uniformly over a line, mapped onto a diamond.
+// The ring size 17*17 = 289 is close to a multiple of 41 (41*7 = 287)
 
+  float3 x = 2.0 * frac(p * C.www) - 1.0;
+  float3 h = abs(x) - 0.5;
+  float3 ox = floor(x + 0.5);
+  float3 a0 = x - ox;
 
+// Normalise gradients implicitly by scaling m
+// Approximation of: m *= inversesqrt( a0*a0 + h*h );
+  m *= 1.79284291400159 - 0.85373472095314 * ( a0*a0 + h*h );
 
-
-
-  float3 x1 = x0 - i1 + C.xxx;
-  float3 x2 = x0 - i2 + C.yyy;
-  float3 x3 = x0 - D.yyy;
-
-
-  i = mod289(i);
-  float4 p = permute( permute( permute(
-             i.z + float4(0.0, i1.z, i2.z, 1.0 ))
-           + i.y + float4(0.0, i1.y, i2.y, 1.0 ))
-           + i.x + float4(0.0, i1.x, i2.x, 1.0 ));
-
-
-
-  float n_ = 0.142857142857;
-  float3 ns = n_ * D.wyz - D.xzx;
-
-  float4 j = p - 49.0 * floor(p * ns.z * ns.z);
-
-  float4 x_ = floor(j * ns.z);
-  float4 y_ = floor(j - 7.0 * x_ );
-
-  float4 x = x_ *ns.x + ns.yyyy;
-  float4 y = y_ *ns.x + ns.yyyy;
-  float4 h = 1.0 - abs(x) - abs(y);
-
-  float4 b0 = float4( x.xy, y.xy );
-  float4 b1 = float4( x.zw, y.zw );
-
-
-
-  float4 s0 = floor(b0)*2.0 + 1.0;
-  float4 s1 = floor(b1)*2.0 + 1.0;
-  float4 sh = -step(h, float4(0.0,0.0,0.0,0.0));
-
-  float4 a0 = b0.xzyw + s0.xzyw*sh.xxyy ;
-  float4 a1 = b1.xzyw + s1.xzyw*sh.zzww ;
-
-  float3 p0 = float3(a0.xy,h.x);
-  float3 p1 = float3(a0.zw,h.y);
-  float3 p2 = float3(a1.xy,h.z);
-  float3 p3 = float3(a1.zw,h.w);
-
-
-  float4 norm = taylorInvSqrt(float4(dot(p0,p0), dot(p1,p1), dot(p2, p2), dot(p3,p3)));
-  p0 *= norm.x;
-  p1 *= norm.y;
-  p2 *= norm.z;
-  p3 *= norm.w;
-
-
-  float4 m = max(0.6 - float4(dot(x0,x0), dot(x1,x1), dot(x2,x2), dot(x3,x3)), 0.0);
-  m = m * m;
-  return 42.0 * dot( m*m, float4( dot(p0,x0), dot(p1,x1),
-                                dot(p2,x2), dot(p3,x3) ) );
+// Compute final noise value at P
+  float3 g;
+  g.x  = a0.x  * x0.x  + h.x  * x0.y;
+  g.yz = a0.yz * x12.xz + h.yz * x12.yw;
+  return 130.0 * dot(m, g);
 }
 
 
@@ -186,7 +154,7 @@ VS_OUTPUT main(VS_INPUT input) {
         output.Position = mul(ViewMatrix, output.Position);
         output.Position = mul(ProjMatrix, output.Position);
 
-        float v = snoise(float3(texPos, 0) / 512.0);
+        float v = snoise(float2(texPos) / 512.0);
         float2 texCoord = float2(v,v);
         //output.v_Color = ColorTable.Sample(ColorTableSampler, float4(texCoord, 0, 0));
         output.v_Color = ColorTable.SampleLevel(ColorTableSampler, texCoord, 0);
