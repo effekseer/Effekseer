9f8a9f419c50c86c12d156a6ccbaf178f0a9915c
durswd <swda.durl@gmail.com>
2022-03-01 22:22:15 +0900

    wip

--- Dev/Editor/EffekseerCore/Data/Value/GradientHDR.cs
+++ Dev/Editor/EffekseerCore/Data/Value/GradientHDR.cs
@@ -5,27 +5,84 @@ using System.Text;
 
 namespace Effekseer.Data.Value
 {
-	class GradientHDR
+	public class GradientHDR
 	{
-		unsafe struct ColorMarker
+		public unsafe struct ColorMarker
 		{
 			public float Position;
 			public fixed float Color[3];
 			public float Intensity;
 		}
 
-		struct AlphaMarker
+		public struct AlphaMarker
 		{
 			public float Position;
 			public float Alpha;
 		}
 
-		class State
+		public class State : ICloneable
 		{
 			public ColorMarker[] ColorMarkers;
 			public AlphaMarker[] AlphaMarkers;
+
+			public object Clone()
+			{
+				var state = new State();
+
+				state.ColorMarkers = (ColorMarker[])ColorMarkers.Clone();
+				state.AlphaMarkers = (AlphaMarker[])AlphaMarkers.Clone();
+
+				return state;
+			}
 		}
 
+		State _value;
 
+		public State Value
+		{
+			get;
+
+		}
+
+		public State GetValue()
+		{
+			return _value;
+		}
+
+		public void SetValue(State value, bool isCombined = false)
+		{
+			value = (State)value.Clone();
+
+			if (value == _value) return;
+
+			var old_value = _value;
+			var new_value = value;
+
+			var cmd = new Command.DelegateCommand(
+				() =>
+				{
+					_value = new_value;
+					CallChanged(new_value, ChangedValueType.Execute);
+				},
+				() =>
+				{
+					_value = old_value;
+					CallChanged(old_value, ChangedValueType.Unexecute);
+				},
+				this,
+				isCombined);
+
+			Command.CommandManager.Execute(cmd);
+		}
+
+		protected void CallChanged(object value, ChangedValueType type)
+		{
+			if (OnChanged != null)
+			{
+				OnChanged(this, new ChangedValueEventArgs(value, type));
+			}
+		}
+
+		public event ChangedValueEventHandler OnChanged;
 	}
 }
---
+++ Dev/Editor/EffekseerCoreGUI/GUI/Component/GradientHDR.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Effekseer.GUI.Component
+{
+	class GradientHDR : Control, IParameterControl
+	{
+		int id = -1;
+
+		Data.Value.GradientHDR binding = null;
+
+		swig.GradientHDRState internalState = null;
+
+		public bool EnableUndo { get; set; } = true;
+
+		public Data.Value.GradientHDR Binding
+		{
+			get
+			{
+				return binding;
+			}
+			set
+			{
+				if (binding == value) return;
+
+				binding = value;
+			}
+		}
+
+		public GradientHDR()
+		{
+			id = Manager.GetUniqueID();
+		}
+
+		public void SetBinding(object o)
+		{
+			Binding = o as Data.Value.GradientHDR;
+		}
+
+		public void FixValue()
+		{
+		}
+
+		public override void Update()
+		{
+			if (Manager.NativeManager.GradientHDR(id, internalState))
+			{
+
+			}
+		}
+
+		void CopyState(swig.GradientHDRState dst, Data.Value.GradientHDR.State src)
+		{
+		}
+
+		unsafe void CopyState(Data.Value.GradientHDR.State dst, swig.GradientHDRState src)
+		{
+			dst.ColorMarkers = new Data.Value.GradientHDR.ColorMarker[src.GetColorCount()];
+			dst.AlphaMarkers = new Data.Value.GradientHDR.AlphaMarker[src.GetAlphaCount()];
+
+			for (int i = 0; i < src.GetColorCount(); i++)
+			{
+				var color = src.GetColorMarkerColor(i);
+				dst.ColorMarkers[i].Color[0] = color.R;
+				dst.ColorMarkers[i].Color[1] = color.G;
+				dst.ColorMarkers[i].Color[2] = color.B;
+				dst.ColorMarkers[i].Position = src.GetColorMarkerPosition(i);
+				dst.ColorMarkers[i].Intensity = src.GetColorMarkerIntensity(i);
+			}
+
+			for (int i = 0; i < src.GetAlphaCount(); i++)
+			{
+				dst.AlphaMarkers[i].Alpha = src.GetAlphaMarkerAlpha(i);
+				dst.AlphaMarkers[i].Position = src.GetAlphaMarkerPosition(i);
+			}
+		}
+	}
+}
