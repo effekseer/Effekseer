68dd434dbc2d898b58203e49b5ce4df1fbad5b65
durswd <swda.durl@gmail.com>
2022-03-14 19:47:28 +0900

    fix

--- Dev/Cpp/Effekseer/Effekseer/Effekseer.Instance.cpp
+++ Dev/Cpp/Effekseer/Effekseer/Effekseer.Instance.cpp
@@ -460,16 +460,6 @@ void Instance::UpdateTransform(float deltaFrame)
 		SIMD::Vec3f localAngle;
 		SIMD::Vec3f localScaling;
 
-		auto localVec = m_pEffectNode->TranslationParam.CalculateTranslationState(translation_values, m_randObject, m_pEffectNode->GetEffect(), m_pContainer->GetRootInstance(), m_LivingTime, m_LivedTime, m_pParent, coordinateSystem, m_pEffectNode->DynamicFactor);
-
-		if (m_pEffectNode->GenerationLocation.EffectsRotation)
-		{
-			// TODO : check rotation(It seems has bugs and it can optimize it)
-			localVec = SIMD::Vec3f::Transform(localVec, m_GenerationLocation.GetRotation());
-		}
-
-		localPosition += localVec;
-
 		if (m_pEffectNode->CommonValues.TranslationBindType == TranslationParentBindType::NotBind_FollowParent ||
 			m_pEffectNode->CommonValues.TranslationBindType == TranslationParentBindType::WhenCreating_FollowParent)
 		{
@@ -496,6 +486,16 @@ void Instance::UpdateTransform(float deltaFrame)
 			SIMD::Vec3f followVelocity = steeringVec_ * deltaFrame * m_pEffectNode->m_effect->GetMaginification();
 			localPosition += followVelocity;
 		}
+
+		auto localVec = m_pEffectNode->TranslationParam.CalculateTranslationState(translation_values, m_randObject, m_pEffectNode->GetEffect(), m_pContainer->GetRootInstance(), m_LivingTime, m_LivedTime, m_pParent, coordinateSystem, m_pEffectNode->DynamicFactor);
+
+		if (m_pEffectNode->GenerationLocation.EffectsRotation)
+		{
+			// TODO : check rotation(It seems has bugs and it can optimize it)
+			localVec = SIMD::Vec3f::Transform(localVec, m_GenerationLocation.GetRotation());
+		}
+
+		localPosition += localVec;
 
 		// Velocitty
 		SIMD::Vec3f localVelocity = SIMD::Vec3f(0, 0, 0);
