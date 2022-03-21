73153bae23aadf9b5ede78fa3cc022e0cdde2d1c
durswd <swda.durl@gmail.com>
2022-03-21 23:35:31 +0900

    1.70Alpha2

--- Dev/Editor/EffekseerCore/Core.cs
+++ Dev/Editor/EffekseerCore/Core.cs
@@ -188,7 +188,7 @@ namespace Effekseer
 
 	public class Core
 	{
-		public const string Version = "1.70α1";
+		public const string Version = "1.70α2";
 
 		public const string OptionFilePath = "config.option.xml";
 
