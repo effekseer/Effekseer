fe64f80b1276ae630245fcf78e16ad9d65cd58d3
durswd <swda.durl@gmail.com>
2022-04-05 13:09:03 +0900

    Fix compile for UE4 on Android

--- Dev/Cpp/Effekseer/Effekseer/Model/ProceduralModelGenerator.cpp
+++ Dev/Cpp/Effekseer/Effekseer/Model/ProceduralModelGenerator.cpp
@@ -49,7 +49,7 @@ struct ProceduralMesh
 			}
 		}
 
-		return std::move(mesh1);
+		return mesh1;
 	}
 };
 
@@ -766,7 +766,7 @@ struct RotatedWireMeshGenerator
 			ret = ProceduralMesh::Combine(std::move(ret), std::move(ribbon));
 		}
 
-		return std::move(ret);
+		return ret;
 	}
 };
 
