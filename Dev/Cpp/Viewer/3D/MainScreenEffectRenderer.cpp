daecaa61d6ee680d60924fd07307242d3d60f0f0
Danil Dubrovsky <forwot163@gmail.com>
2022-05-23 14:34:04 +0200

    Moved grid and culling debug rendering to editor c# side

--- Dev/Cpp/Viewer/3D/MainScreenEffectRenderer.cpp
+++ Dev/Cpp/Viewer/3D/MainScreenEffectRenderer.cpp
@@ -2,8 +2,6 @@
 #include "MainScreenEffectRenderer.h"
 #include "../3D/EffectRenderer.h"
 #include "../EffekseerRendererCommon/EffekseerRenderer.PngTextureLoader.h"
-#include "../EffekseerTool/EffekseerTool.Culling.h"
-#include "../EffekseerTool/EffekseerTool.Grid.h"
 #include "../EffekseerTool/EffekseerTool.Guide.h"
 #include "../Graphics/GraphicsDevice.h"
 #include <spdlog/sinks/basic_file_sink.h>
@@ -21,35 +19,6 @@ MainScreenEffectRenderer::~MainScreenEffectRenderer()
 	}
 }
 
-void MainScreenEffectRenderer::OnAfterClear()
-{
-	EffectRenderer::OnAfterClear();
-	
-	const auto cameraMat = renderer_->GetCameraMatrix();
-	const auto projMat = renderer_->GetProjectionMatrix();
-
-	if (parameter_.RenderingMethod != Effekseer::Tool::RenderingMethodType::Overdraw)
-	{
-		if (IsGridShown)
-		{
-			grid_->SetLength(GridLength);
-			grid_->IsShownXY = IsGridXYShown;
-			grid_->IsShownXZ = IsGridXZShown;
-			grid_->IsShownYZ = IsGridYZShown;
-			grid_->Rendering(GridColor, IsRightHand, cameraMat, projMat);
-		}
-
-		{
-			culling_->IsShown = IsCullingShown;
-			culling_->Radius = CullingRadius;
-			culling_->X = CullingPosition.X;
-			culling_->Y = CullingPosition.Y;
-			culling_->Z = CullingPosition.Z;
-			culling_->Rendering(IsRightHand, cameraMat, projMat);
-		}
-	}
-}
-
 void MainScreenEffectRenderer::OnBeforePostprocess()
 {
 	if (RendersGuide)
@@ -60,14 +29,6 @@ void MainScreenEffectRenderer::OnBeforePostprocess()
 
 bool MainScreenEffectRenderer::OnAfterInitialize()
 {
-	grid_ = std::shared_ptr<::EffekseerRenderer::Grid>(::EffekseerRenderer::Grid::Create(graphics_->GetGraphics()->GetGraphicsDevice()));
-	if (grid_ == nullptr)
-	{
-		return false;
-	}
-
-	spdlog::trace("OK Grid");
-
 	guide_ = std::shared_ptr<::EffekseerRenderer::Guide>(::EffekseerRenderer::Guide::Create(graphics_->GetGraphics()->GetGraphicsDevice()));
 	if (guide_ == nullptr)
 	{
@@ -76,15 +37,14 @@ bool MainScreenEffectRenderer::OnAfterInitialize()
 
 	spdlog::trace("OK Guide");
 
-	culling_ = std::shared_ptr<::EffekseerRenderer::Culling>(::EffekseerRenderer::Culling::Create(graphics_->GetGraphics()->GetGraphicsDevice()));
-	if (culling_ == nullptr)
+
+	lineRenderer_ = std::make_shared<LineRenderer>(graphics_->GetGraphics()->GetGraphicsDevice());
+	if(lineRenderer_ == nullptr)
 	{
 		return false;
 	}
-
-	lineRenderer_ = std::make_shared<Effekseer::Tool::LineRenderer>(graphics_->GetGraphics()->GetGraphicsDevice());
-
-	spdlog::trace("OK Culling");
+	
+	spdlog::trace("OK Generic Line Renderer");
 
 	textureLoader_ = renderer_->CreateTextureLoader();
 
--- Dev/Editor/EffekseerCoreGUI/GUI/Viewer.cs
+++ Dev/Editor/EffekseerCoreGUI/GUI/Viewer.cs
@@ -320,10 +320,109 @@ namespace Effekseer.GUI
 
 			if (Core.Option.RenderingMode != OptionValues.RenderMode.Overdraw)
 			{
+				RenderGrid(cameraMatrix,projectionMatrix);
 				RenderKillRulesPreview(cameraMatrix, projectionMatrix);
+				RenderCullingPreview(cameraMatrix, projectionMatrix);
 			}
 		}
 
+		private void RenderGrid(Matrix44F cameraMatrix, Matrix44F projectionMatrix)
+		{
+			if(!Core.Option.IsGridShown) return;
+			
+			Color gridColor = new Color
+			{
+				R = (byte)Core.Option.GridColor.R,
+				G = (byte)Core.Option.GridColor.G,
+				B = (byte)Core.Option.GridColor.B,
+				A = (byte)Core.Option.GridColor.A
+			};
+			float gridLength = Core.Option.GridLength;
+
+			EffectRenderer.StartRenderingLines();
+
+			if (Core.Option.IsXZGridShown)
+			{
+				for (int i = -5; i <= 5; i++)
+				{
+					EffectRenderer.AddLine(i * gridLength, 0, gridLength * 5.0F,
+						i * gridLength, 0, -gridLength * 5.0F,
+						gridColor);
+					EffectRenderer.AddLine(gridLength * 5.0F, 0, i * gridLength,
+						-gridLength * 5.0F, 0, i * gridLength,
+						gridColor);
+				}
+			}
+
+			if (Core.Option.IsXYGridShown)
+			{
+				for (int i = -5; i <= 5; i++)
+				{
+					EffectRenderer.AddLine(i * gridLength, gridLength * 5.0F, 0,
+						i * gridLength, -gridLength * 5.0F, 0,
+						gridColor);
+					EffectRenderer.AddLine(gridLength * 5.0F, i * gridLength, 0,
+						-gridLength * 5.0F, i * gridLength, 0,
+						gridColor);
+				}
+			}
+			
+			if (Core.Option.IsYZGridShown)
+			{
+				for (int i = -5; i <= 5; i++)
+				{
+					EffectRenderer.AddLine(0, i * gridLength, gridLength * 5.0F,
+						0, i * gridLength, -gridLength * 5.0F,
+						gridColor);
+					EffectRenderer.AddLine(0, gridLength * 5.0F, i * gridLength,
+						0, -gridLength * 5.0F, i * gridLength,
+						gridColor);
+				}
+			}
+			
+			EffectRenderer.AddLine(0, 0.001F, 0, gridLength, 0.001F, 0,
+				new Color(255, 0, 0, 255));
+			EffectRenderer.AddLine(0, 0, 0, 0, gridLength, 0,
+				new Color(0, 255, 0, 255));
+			EffectRenderer.AddLine(0, 0.001F, 0, 0, 0.001F, gridLength,
+				new Color(0, 0, 255, 255));
+			
+			EffectRenderer.EndRenderingLines(cameraMatrix, projectionMatrix);
+		}
+		
+		private void RenderCullingPreview(Matrix44F cameraMatrix, Matrix44F projectionMatrix)
+		{
+			if (Core.Culling.Type.Value != Data.EffectCullingValues.ParamaterType.Sphere) return;
+
+			Color sphereColor = new Color(255, 255, 255, 255);
+			EffectRenderer.StartRenderingLines();
+
+			float cullingRadius = Core.Culling.Sphere.Radius.Value;
+			float sphereX = Core.Culling.Sphere.Location.X;
+			float sphereY = Core.Culling.Sphere.Location.Y;
+			float sphereZ = Core.Culling.Sphere.Location.Z;
+			
+			for (int y = -3; y <= 3; y++)
+			{
+				float ylen = cullingRadius * (y / 4.0f);
+				float radius = (float)Math.Sqrt(cullingRadius * cullingRadius - ylen * ylen);
+
+				for (int r = 0; r < 9; r++)
+				{
+					float a0 = 3.1415f * 2.0f / 9.0f * r;
+					float a1 = 3.1415f * 2.0f / 9.0f * (r + 1.0f);
+					
+					EffectRenderer.AddLine(
+						(float)(sphereX + Math.Sin(a0) * radius), sphereY + ylen, (float)(sphereZ + Math.Cos(a0) * radius),
+						(float)(sphereX + Math.Sin(a1) * radius), sphereY + ylen, (float)(sphereZ + Math.Cos(a1) * radius),
+						sphereColor
+						);
+				}
+			}
+
+			EffectRenderer.EndRenderingLines(cameraMatrix, projectionMatrix);
+		}
+		
 		private void RenderKillRulesPreview(Matrix44F cameraMatrix, Matrix44F projectionMatrix)
 		{
 			if (Core.SelectedNode == null || !(Core.SelectedNode is Data.Node)) return;
@@ -495,37 +594,6 @@ namespace Effekseer.GUI
 				EffectRenderer.SetParameter(renderParam);
 
 				EffectRenderer.LoadBackgroundImage(Core.Environment.Background.BackgroundImage.AbsolutePath);
-
-				EffectRenderer.GridColor = new swig.Color
-				{
-					R = (byte)Core.Option.GridColor.R,
-					G = (byte)Core.Option.GridColor.G,
-					B = (byte)Core.Option.GridColor.B,
-					A = (byte)Core.Option.GridColor.A
-				};
-
-				EffectRenderer.GridLength = Core.Option.GridLength;
-				EffectRenderer.IsGridShown = Core.Option.IsGridShown;
-				EffectRenderer.IsGridXYShown = Core.Option.IsXYGridShown;
-				EffectRenderer.IsGridXZShown = Core.Option.IsXZGridShown;
-				EffectRenderer.IsGridYZShown = Core.Option.IsYZGridShown;
-
-				if (Core.Culling.Type.Value == Data.EffectCullingValues.ParamaterType.Sphere)
-				{
-					EffectRenderer.IsCullingShown = true;
-					EffectRenderer.CullingRadius = Core.Culling.Sphere.Radius.Value;
-					EffectRenderer.CullingPosition.X = Core.Culling.Sphere.Location.X;
-					EffectRenderer.CullingPosition.Y = Core.Culling.Sphere.Location.Y;
-					EffectRenderer.CullingPosition.Z = Core.Culling.Sphere.Location.Z;
-				}
-				else if (Core.Culling.Type.Value == Data.EffectCullingValues.ParamaterType.None)
-				{
-					EffectRenderer.IsCullingShown = false;
-					EffectRenderer.CullingRadius = 0.0f;
-					EffectRenderer.CullingPosition.X = 0.0f;
-					EffectRenderer.CullingPosition.Y = 0.0f;
-					EffectRenderer.CullingPosition.Z = 0.0f;
-				}
 
 				ViewPointController.SetMouseInverseFlag(
 					Core.Option.MouseRotInvX,
