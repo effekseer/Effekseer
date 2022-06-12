bfe3e3710b9caec73e10f0e87370d7bdaa3c0ac0
durswd <swda.durl@gmail.com>
2022-06-13 01:57:05 +0900

    1.62e

--- Dev/Editor/EffekseerCore/Core.cs
+++ Dev/Editor/EffekseerCore/Core.cs
@@ -188,7 +188,7 @@ namespace Effekseer
 
 	public class Core
 	{
-		public const string Version = "1.62d";
+		public const string Version = "1.62e";
 
 		public const string OptionFilePath = "config.option.xml";
 
