9274bc6a8218d9d8e9bfa5916b085feb126ed877
durswd <swda.durl@gmail.com>
2022-09-23 02:03:37 +0900

    fix

--- build.py
+++ build.py
@@ -192,8 +192,8 @@ if env['IGNORE_BUILD'] == '0':
         call('rm -rf -r Dev/release/osx.10.11-x64')
 
     elif isWin():
-        call('"' + msbuild_path + '"' + ' Dev/Editor/EffekseerCore/EffekseerCore.csproj /t:build /p:Configuration=Release /p:Platform=x64')
-        call('"' + msbuild_path + '"' + ' Dev/Editor/Effekseer/Effekseer.csproj /t:build /p:Configuration=Release /p:Platform=x64')
+        call('dotnet build Dev/Editor/Effekseer/Effekseer.csproj')
+        call('dotnet publish Dev/Editor/Effekseer/Effekseer.csproj -c Release --self-contained -r win-x64')
     else:
         call('dotnet build Dev/Editor/Effekseer/Effekseer.csproj')
         call('dotnet publish Dev/Editor/Effekseer/Effekseer.csproj -c Release --self-contained -r linux-x64')
