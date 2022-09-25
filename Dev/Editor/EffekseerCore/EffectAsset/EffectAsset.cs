92c77a1d71fcc5daf3d1795866ea116e1a935c47
durswd <swda.durl@gmail.com>
2022-09-25 21:28:59 +0900

    Add EffectAssetEditorContext

--- Dev/Editor/EffekseerCore/EffectAsset/EffectAsset.cs
+++ Dev/Editor/EffekseerCore/EffectAsset/EffectAsset.cs
@@ -7,14 +7,80 @@ using Newtonsoft.Json;
 
 namespace Effekseer.EffectAsset
 {
-	class EffectAsset
+	public class EffectAssetEnvironment : PartsTreeSystem.Environment
 	{
-		public Node Root = new RootNode();
-
-		public void New()
+		Dictionary<PartsTreeSystem.Asset, string> pathes = new Dictionary<PartsTreeSystem.Asset, string>();
+		public override PartsTreeSystem.Asset GetAsset(string path)
 		{
-			Root = new RootNode();
-			Root.AddChild(new ParticleNode());
+			if (pathes.ContainsValue(path))
+			{
+				return pathes.Where(_ => _.Value == path).FirstOrDefault().Key;
+			}
+			var text = System.IO.File.ReadAllText(path);
+			var nodeTreeGroup = PartsTreeSystem.NodeTreeGroup.Deserialize(text);
+
+			pathes.Add(nodeTreeGroup, path);
+			return nodeTreeGroup;
+		}
+
+		public override string GetAssetPath(PartsTreeSystem.Asset asset)
+		{
+			if (pathes.TryGetValue(asset, out var path))
+			{
+				return System.IO.Path.Combine(System.IO.Directory.GetCurrentDirectory(), path);
+			}
+			return System.IO.Directory.GetCurrentDirectory();
+		}
+	}
+
+	public class EffectAssetEditorContext
+	{
+		public PartsTreeSystem.NodeTree NodeTree { get; private set; }
+
+		public PartsTreeSystem.CommandManager CommandManager { get; private set; }
+
+		public PartsTreeSystem.NodeTreeGroupEditorProperty EditorProperty { get; private set; }
+
+		EffectAssetEnvironment env;
+
+		public EffectAssetEditorContext(PartsTreeSystem.NodeTree nodeTree, PartsTreeSystem.NodeTreeGroupEditorProperty editorProperty, EffectAssetEnvironment env)
+		{
+			NodeTree = nodeTree;
+			EditorProperty = editorProperty;
+			this.env = env;
+			CommandManager = new PartsTreeSystem.CommandManager();
+		}
+
+		public void Undo()
+		{
+			CommandManager.Undo(env);
+			EditorProperty.Rebuild();
+		}
+
+		public void Redo()
+		{
+			CommandManager.Redo(env);
+			EditorProperty.Rebuild();
+		}
+	}
+
+	public class EffectAsset
+	{
+		public PartsTreeSystem.NodeTreeGroup NodeTreeGroup { get; private set; }
+
+		public EffectAssetEditorContext CreateEditorContext(EffectAssetEnvironment env)
+		{
+			var nodeTree = PartsTreeSystem.Utility.CreateNodeFromNodeTreeGroup(NodeTreeGroup, env);
+			var editorProperty = new PartsTreeSystem.NodeTreeGroupEditorProperty(NodeTreeGroup, env);
+			var context = new EffectAssetEditorContext(nodeTree, editorProperty, env);
+			return context;
+		}
+
+		public void New(EffectAssetEnvironment env)
+		{
+			NodeTreeGroup = new PartsTreeSystem.NodeTreeGroup();
+			var rootNodeId = NodeTreeGroup.Init(typeof(RootNode), env);
+			NodeTreeGroup.AddNode(rootNodeId, typeof(Node), env);
 		}
 	}
 
