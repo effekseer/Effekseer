52a0d932120d29c16a1bec66904ca5c5c126672d
durswd <swda.durl@gmail.com>
2022-09-26 19:46:16 +0900

    Add math types

---
+++ Dev/Editor/EffekseerCore/Math/Vector3F.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Effekseer
+{
+	public struct Vector3F
+	{
+		public float X;
+		public float Y;
+		public float Z;
+	}
+}
