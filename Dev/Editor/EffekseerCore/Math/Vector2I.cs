c612c67b1c084272ae9a4b017f4a3ebc4deb8811
durswd <swda.durl@gmail.com>
2022-09-26 19:46:16 +0900

    Add math types

---
+++ Dev/Editor/EffekseerCore/Math/Vector2I.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Effekseer
+{
+	public struct Vector2I
+	{
+		public int X;
+		public int Y;
+	}
+}
---
+++ Dev/Editor/EffekseerCore/Math/Vector3I.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Effekseer
+{
+	public struct Vector3I
+	{
+		public int X;
+		public int Y;
+		public int Z;
+	}
+}
