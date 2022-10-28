e7a7a44598d4eca19cbe21d013c445880aab8c8a
mewmew-tea <kd1285579@st.kobedenshi.ac.jp>
2022-10-28 21:34:22 +0900

    Improve uses of elementGetterSetters and generation of gui infos

--- Dev/Editor/EffekseerCoreGUI/GUI/Inspector/Inspector.cs
+++ Dev/Editor/EffekseerCoreGUI/GUI/Inspector/Inspector.cs
@@ -77,6 +77,11 @@ namespace Effekseer.GUI.Inspector
 
 		private static void UpdateVisiblityControllers(object target)
 		{
+			if (target==null)
+			{
+				return;
+			}
+
 			var fields = target.GetType().GetFields();
 			foreach (var field in fields)
 			{
@@ -101,247 +106,267 @@ namespace Effekseer.GUI.Inspector
 			generate = (object tgt) =>
 			{
 				InspectorGuiInfo info = new InspectorGuiInfo();
-				if (tgt == null ||
-					tgt.GetType().IsPrimitive ||
-					tgt.GetType() == typeof(string) ||
-					tgt.GetType().IsEnum)
+				if (tgt == null)
 				{
 					return info;
 				}
-				var fields = tgt.GetType().GetFields();
-				foreach (var field in fields)
+				var type = tgt.GetType();
+
+				// TODO: when guiFuncs work are completed, this should be only "GuiDictionary.HasFunction(type)"
+				bool hasGuiFunction = type.IsPrimitive ||
+							type == typeof(string) ||
+							type.IsEnum ||
+							GuiDictionary.HasFunction(type);
+				if (hasGuiFunction)
+				{
+					return info;
+				}
+
+				// fields in tgt
+				var fieldsInTgt = tgt.GetType().GetFields();
+				foreach (var fieldInTgt in fieldsInTgt)
 				{
 					// visiblity controller attribute
 					UpdateVisiblityControllers(tgt);
+					info.subElement.Add(generate(fieldInTgt.GetValue(tgt)));
 
-					if (!GuiDictionary.HasFunction(field.GetType()) && !field.GetType().IsPrimitive)
-					{
-						info.subElement.Add(generate(field.GetValue(tgt)));
-					}
 				}
 				return info;
 			};
+
+
 			var fields = target.GetType().GetFields();
 			foreach (var field in fields)
 			{
-				FieldGuiInfoList.Add(generate(field.GetValue(target)));
+				var tgt = field.GetValue(target);
+				{
+					UpdateVisiblityControllers(tgt);
+				}
+				
+				FieldGuiInfoList.Add(generate(tgt));
 			}
 		}
 
 		private static void UpdateObjectGuis(EffectAsset.EffectAssetEditorContext context
-			, object targetNode, object targetObject, List<InspectorGuiInfo> guiInfos)
+			, object targetNode, ElementGetterSetterArray elementGetterSetterArray, InspectorGuiInfo guiInfo)
 		{
-			var getterSetters = new FieldGetterSetter[1];
-			getterSetters[0] = new FieldGetterSetter();
+			var field = elementGetterSetterArray.GetFieldInfo();
 
-			// エディタに表示する変数群
-			var fields = targetObject.GetType().GetFields();
-			UpdateVisiblityControllers(targetObject);
+			bool isValueChanged = false;
 
-			bool isFirstField = true;
-
-
-			for (int i = 0; i < fields.Length; ++i)
+			var prop = context.EditorProperty.Properties.FirstOrDefault(_ => _.InstanceID == ((EffectAsset.Node)targetNode)?.InstanceID);
+			if (prop != null)
 			{
-				var field = fields[i];
-				field.GetCustomAttributes();
-				getterSetters[0].Reset(targetObject, field);
+				isValueChanged = prop.IsValueEdited(elementGetterSetterArray.Names);
+			}
 
-				bool isValueChanged = false;
-				var prop = context.EditorProperty.Properties.FirstOrDefault(_ => _.InstanceID == ((EffectAsset.Node)targetNode)?.InstanceID);
-				if (prop != null)
+			var value = elementGetterSetterArray.GetValue();
+			var name = elementGetterSetterArray.GetName();
+
+			if (value == null)
+			{
+				// TODO : nullどうする？
+				//Manager.NativeManager.Text("null : " + field.GetType().ToString());
+				return;
+			}
+
+
+			// 配列かリストの時、エレメントの型を取得する
+			var valueType = value.GetType();
+			bool isList = valueType is IList;
+			if (isList)
+			{
+				valueType = valueType.GetElementType();
+			}
+			else if (valueType.IsGenericType && valueType.GetGenericTypeDefinition() == typeof(List<>))
+			{
+				valueType = valueType.GetGenericArguments()[0];
+			}
+
+			var guiFunctionKey = valueType;
+
+			// when valueType is enum, the key is System.Enum 
+			if (valueType.IsEnum)
+			{
+				guiFunctionKey = typeof(System.Enum);
+			}
+
+
+			// key attrs
+			string description = string.Empty;
+			{
+				var attr = (KeyAttribute)field.GetCustomAttribute(typeof(KeyAttribute));
+				if (attr != null)
 				{
-					isValueChanged = prop.IsValueEdited(getterSetters.Select(_ => _.GetName()).ToArray());
-				}
+					string key = attr.key + "_Name";
 
-				var getterSetter = getterSetters.Last();
-				var value = getterSetter.GetValue();
-				var name = getterSetter.GetName();
-
-				if (value == null)
-				{
-					// TODO : nullどうする？
-					//Manager.NativeManager.Text("null : " + field.GetType().ToString());
-					continue;
-				}
-
-
-				// 配列かリストの時、エレメントの型を取得する
-				var valueType = value.GetType();
-				bool isArray = valueType.IsArray;
-				if (isArray)
-				{
-					valueType = valueType.GetElementType();
-				}
-				else if (valueType.IsGenericType && valueType.GetGenericTypeDefinition() == typeof(List<>))
-				{
-					valueType = valueType.GetGenericArguments()[0];
-				}
-
-				var guiFunctionKey = valueType;
-
-				// the key about enums gui function is System.Enum
-				if (valueType.IsEnum)
-				{
-					guiFunctionKey = typeof(System.Enum);
-				}
-
-
-				// key attrs
-				string description = string.Empty;
-				{
-					var attr = (KeyAttribute)field.GetCustomAttribute(typeof(KeyAttribute));
-					if (attr != null)
+					if (MultiLanguageTextProvider.HasKey(key))
 					{
-						string key = attr.key + "_Name";
+						name = MultiLanguageTextProvider.GetText(key);
+					}
 
-						if (MultiLanguageTextProvider.HasKey(key))
-						{
-							name = MultiLanguageTextProvider.GetText(key);
-						}
-
-						key = attr.key + "_Desc";
-						if (MultiLanguageTextProvider.HasKey(key))
-						{
-							description = MultiLanguageTextProvider.GetText(key);
-						}
+					key = attr.key + "_Desc";
+					if (MultiLanguageTextProvider.HasKey(key))
+					{
+						description = MultiLanguageTextProvider.GetText(key);
 					}
 				}
-				
-				// VisiblityControlledAttributes
+			}
+			// VisiblityControlledAttributes
+			{
 				bool isVisible = true;
+				var attr = (EffectAsset.VisiblityControlledAttribute)field.GetCustomAttribute(typeof(EffectAsset.VisiblityControlledAttribute));
+				if (attr != null)
 				{
-					var attr = (EffectAsset.VisiblityControlledAttribute)field.GetCustomAttribute(typeof(EffectAsset.VisiblityControlledAttribute));
-					if (attr != null)
+					if (VisiblityControllers.ContainsKey(attr.ID))
 					{
-						if (VisiblityControllers.ContainsKey(attr.ID))
-						{
-							var controllerValue = (int)VisiblityControllers[attr.ID];
-							isVisible = (attr.Value == controllerValue);
-						}
+						var controllerValue = (int)VisiblityControllers[attr.ID];
+						isVisible = (attr.Value == controllerValue);
 					}
 				}
 				if (!isVisible)
 				{
-					continue;
+					return;
 				}
+			}
 
 
-				//if (NodeParameters.Contains(value))
+			// update subfields
+			int i = 0;
+			var subFields = valueType.GetFields();
+			bool shownSubFields = false;
+			foreach (var f in subFields)
+			{
 				if (!GuiDictionary.HasFunction(guiFunctionKey) &&
-					value.GetType().GetFields().Length > 0 &&
-					!value.GetType().IsEnum &&
-					guiInfos[i].subElement.Count >= i + 1)
+				value.GetType().GetFields().Length > 0 &&
+				!value.GetType().IsEnum &&
+				guiInfo.subElement.Count > i)
 				{
-					UpdateObjectGuis(context, targetNode, value, guiInfos[i].subElement);
-					continue;
+					UpdateVisiblityControllers(value);
+					elementGetterSetterArray.Push(value, f);
+					UpdateObjectGuis(context, targetNode, elementGetterSetterArray, guiInfo.subElement[i]);
+					elementGetterSetterArray.Pop();
+					shownSubFields = true;
 				}
+				++i;
+			}
+			if (shownSubFields)
+			{
+				return;
+			}
 
-				// display name(left side of table)
-				Manager.NativeManager.TableNextRow();
-				Manager.NativeManager.TableNextColumn();
-				// TODO : Separatorで区切るのはAssetなどの単位にする
-				// Make not separate first row
-				bool isShowHorizonalSeparator = Manager.NativeManager.TableGetRowIndex() >= 2 && isFirstField;
-				if (isShowHorizonalSeparator)
-				{
-					Manager.NativeManager.Separator();
-				}
+
+			// display name(left side of table)
+			Manager.NativeManager.TableNextRow();
+			Manager.NativeManager.TableNextColumn();
+			// TODO : Separatorで区切るのはAssetなどの単位にする
+			// Make not separate first row
+			bool isShowHorizonalSeparator = Manager.NativeManager.TableGetRowIndex() >= 2;
+			if (isShowHorizonalSeparator)
+			{
+				Manager.NativeManager.Separator();
+			}
+
+			//// TODO: for debugging. this should be delete.
+			//name = (isValueChanged ? "*" : "") + name + " " + guiInfo.Id;
+
+			Manager.NativeManager.Text(name);
+			// Description
+			if (Manager.NativeManager.IsItemHovered())
+			{
+				Manager.NativeManager.BeginTooltip();
+
 				Manager.NativeManager.Text(name);
-				// Description
-				if (Manager.NativeManager.IsItemHovered())
+				Manager.NativeManager.Separator();
+				Manager.NativeManager.Text(description);
+
+				Manager.NativeManager.EndTooltip();
+			}
+
+
+			// display field(right side of table)
+			Manager.NativeManager.TableNextColumn();
+			// TODO : Separatorで区切るのはAssetなどの単位にする
+			if (isShowHorizonalSeparator)
+			{
+				Manager.NativeManager.Separator();
+			}
+
+			//bool opened = Manager.NativeManager.CollapsingHeader(label);
+
+			// TODO : make ignore "public List<Node> Children = new List<Node>();" node member.
+			if (GuiDictionary.HasFunction(guiFunctionKey))
+			{
+				if (isValueChanged)
 				{
-					Manager.NativeManager.BeginTooltip();
-
-					Manager.NativeManager.Text(name);
-					Manager.NativeManager.Separator();
-					Manager.NativeManager.Text(description);
-
-					Manager.NativeManager.EndTooltip();
+					Manager.NativeManager.PushStyleColor(swig.ImGuiColFlags.Border, 0x77ffff11);
+					Manager.NativeManager.PushStyleVar(swig.ImGuiStyleVarFlags.FrameBorderSize, 1);
 				}
 
+				var func = GuiDictionary.GetFunction(guiFunctionKey);
 
-				// display field(right side of table)
-				Manager.NativeManager.TableNextColumn();
-				// TODO : Separatorで区切るのはAssetなどの単位にする
-				if (isShowHorizonalSeparator)
+				if (isList)
 				{
-					Manager.NativeManager.Separator();
-				}
+					IList arrayValue = (IList)value;
 
-				//bool opened = Manager.NativeManager.CollapsingHeader(label);
-
-				// TODO : make ignore "public List<Node> Children = new List<Node>();" node member.
-				if (GuiDictionary.HasFunction(guiFunctionKey))
-				{
-					if (isValueChanged)
+					// GuiIdが足りなければ、すべて再生成
+					// GenerateFieldGuiIdsの中でやりたいが、型情報からは要素数が分からないのでここで生成。
+					if (arrayValue.Count > guiInfo.subElement.Count())
 					{
-						Manager.NativeManager.PushStyleColor(swig.ImGuiColFlags.Border, 0x77ffff11);
-						Manager.NativeManager.PushStyleVar(swig.ImGuiStyleVarFlags.FrameBorderSize, 1);
-					}
+						guiInfo.subElement.Clear();
 
-					var func = GuiDictionary.GetFunction(guiFunctionKey);
-
-					if (isArray)
-					{
-						Array arrayValue = (Array)value;
-
-						// GuiIdが足りなければ、すべて再生成
-						// GenerateFieldGuiIdsの中でやりたいが、型情報からは要素数が分からないのでここで生成。
-						if (arrayValue.GetLength(0) > guiInfos[i].subElement.Count())
-						{
-							guiInfos[i].subElement.Clear();
-
-							foreach (var v in arrayValue)
-							{
-								FieldGuiInfoList[i].subElement.Add(new InspectorGuiInfo());
-							}
-						}
-
-						int j = 0;
-						bool isEdited = false;
 						foreach (var v in arrayValue)
 						{
-							string id = guiInfos[i].subElement[j].Id;
-							var result = func(v, id);
-							if (result.isEdited)
-							{
-								if (valueType.IsValueType)
-								{
-									arrayValue.SetValue(result.value, j);
-								}
-								isEdited = true;
-							}
-							++j;
-						}
-						if (isEdited)
-						{
-							field.SetValue(targetObject, arrayValue);
-							context.CommandManager.NotifyEditFields((PartsTreeSystem.IInstance)targetNode);
-						}
-					}
-					else
-					{
-						string id = guiInfos[i].Id;
-						var result = func(value, id);
-						if (result.isEdited)
-						{
-							field.SetValue(targetObject, result.value);
-							context.CommandManager.NotifyEditFields((PartsTreeSystem.IInstance)targetNode);
+							FieldGuiInfoList[i].subElement.Add(new InspectorGuiInfo());
 						}
 					}
 
-					if (isValueChanged)
+					int j = 0;
+					bool isEdited = false;
+					foreach (var v in arrayValue)
 					{
-						Manager.NativeManager.PopStyleColor();
-						Manager.NativeManager.PopStyleVar();
+						string id = guiInfo.subElement[j].Id;
+						elementGetterSetterArray.Push(arrayValue, j);
+						var result = func(v, id);
+						if (result.isEdited)
+						{
+							if (valueType.IsValueType)
+							{
+								elementGetterSetterArray.SetValue(result.value);
+							}
+							isEdited = true;
+						}
+						elementGetterSetterArray.Pop();
+						++j;
+					}
+					if (isEdited)
+					{
+						field.SetValue(targetNode, arrayValue);
+						context.CommandManager.NotifyEditFields((PartsTreeSystem.IInstance)targetNode);
 					}
 				}
 				else
 				{
-					Manager.NativeManager.Text("No Regist : " + value.GetType().ToString() + " " + name);
+					string id = guiInfo.Id;
+					var result = func(value, id);
+					if (result.isEdited)
+					{
+						elementGetterSetterArray.SetValue(result.value);
+						context.CommandManager.NotifyEditFields((PartsTreeSystem.IInstance)targetNode);
+					}
+
 				}
-				isFirstField = false;
+
+				if (isValueChanged)
+				{
+					Manager.NativeManager.PopStyleColor();
+					Manager.NativeManager.PopStyleVar();
+				}
+			}
+			else
+			{
+				Manager.NativeManager.Text("No Regist : " + value.GetType().ToString() + " " + name);
+
 			}
 		}
 
@@ -369,14 +394,6 @@ namespace Effekseer.GUI.Inspector
 			}
 			LastTarget = targetNode;
 
-			var commandManager = CoreContext.SelectedEffect.Context.CommandManager;
-			var nodeTreeGroup = CoreContext.SelectedEffect.Asset.NodeTreeAsset;
-			var nodeTree = CoreContext.SelectedEffect.Context.NodeTree;
-			//var partsList = editorState.PartsList;
-			var env = CoreContext.Environment;
-			//var nodeTreeGroupEditorProperty = context.EditorProperty;
-
-
 
 			if (Manager.NativeManager.BeginTable("Table", 2, 
 				swig.TableFlags.Resizable |
@@ -391,14 +408,25 @@ namespace Effekseer.GUI.Inspector
 				Manager.NativeManager.TableSetColumnIndex(1);
 				Manager.NativeManager.PushItemWidth(-1);
 
-				UpdateObjectGuis(context, targetNode, targetNode, FieldGuiInfoList);
+
+				var fields = targetNode.GetType().GetFields();
+				int i = 0;
+				ElementGetterSetterArray elementGetterSetterArray = new ElementGetterSetterArray();
+				foreach (var field in fields)
+				{
+					elementGetterSetterArray.Push(targetNode, field);
+					UpdateVisiblityControllers(targetNode);
+					UpdateObjectGuis(context, targetNode, elementGetterSetterArray, FieldGuiInfoList[i]);
+					elementGetterSetterArray.Pop();
+					++i;
+				}
 			}
 			Manager.NativeManager.EndTable();
 			Manager.NativeManager.Separator();
 		}
 	}
 
-	class InspectorEditable : Node
+	public class InspectorEditable : EffectAsset.Node
 	{
 		//[Key(key = "Node_Name")]
 		//public string Name = string.Empty;
