119dd5d43757d2a85ea2da5a3307540aa003a13c
durswd <swda.durl@gmail.com>
2022-10-29 13:54:42 +0900

    You ca now use ASAN

--- Script/GenerateProjects_Sanitizer_MSVC.bat
+++ Script/GenerateProjects_Sanitizer_MSVC.bat
@@ -5,10 +5,6 @@ mkdir ..\build_sanitizer
 
 cd /d ..\build_sanitizer
 
-cmake -A Win32 -D BUILD_VIEWER=ON -D BUILD_EDITOR=ON -D BUILD_TEST=ON ../
-
-cd /d %~dp0
-
-python GenerateProjects_Sanitizer_MSVC.py
+cmake -A x64 -D BUILD_VIEWER=ON -D BUILD_EDITOR=ON -D BUILD_TEST=ON -D SANITIZE_ENABLED=ON ../
 
 pause
