1b1797cadb2dcfe93ef77eaa05ec391e247b1679
durswd <swda.durl@gmail.com>
2022-11-03 01:17:20 +0900

    Support ColorGUI

--- Dev/Editor/EffekseerCoreGUI/GUI/Inspector/InspectorGUI.cs
+++ Dev/Editor/EffekseerCoreGUI/GUI/Inspector/InspectorGUI.cs
@@ -32,6 +32,7 @@ namespace Effekseer.GUI.Inspector
 				{ typeof(Vector3F), GuiVector3F },
 				{ typeof(System.Enum), GuiEnum },
 				{ typeof(EffectAsset.Gradient), GuiGradient },
+				{ typeof(EffectAsset.Color), GuiColor },
 			};
 		}
 
@@ -304,6 +305,37 @@ namespace Effekseer.GUI.Inspector
 					}
 				}
 				Manager.NativeManager.EndCombo();
+			}
+
+			return ret;
+		}
+
+		private InspectorGuiResult GuiColor(object value, InspectorGuiState state)
+		{
+			InspectorGuiResult ret = new InspectorGuiResult();
+
+			if (value is EffectAsset.Color color)
+			{
+				var colorSpace = (color.ColorSpace == Data.ColorSpace.RGBA)
+				? (swig.ColorEditFlags.DisplayRGB | swig.ColorEditFlags.InputRGB)
+				: (swig.ColorEditFlags.DisplayHSV | swig.ColorEditFlags.InputHSV);
+
+				float[] internalValue = new float[] { 1.0f, 1.0f, 1.0f, 1.0f };
+				internalValue[0] = color.V1 / 255.0f;
+				internalValue[1] = color.V2 / 255.0f;
+				internalValue[2] = color.V3 / 255.0f;
+				internalValue[3] = color.A / 255.0f;
+
+				if (Manager.NativeManager.ColorEdit4(state.Id, internalValue, colorSpace))
+				{
+					color.V1 = (int)Math.Round(internalValue[0] * 255, MidpointRounding.AwayFromZero);
+					color.V2 = (int)Math.Round(internalValue[1] * 255, MidpointRounding.AwayFromZero);
+					color.V3 = (int)Math.Round(internalValue[2] * 255, MidpointRounding.AwayFromZero);
+					color.A = (int)Math.Round(internalValue[3] * 255, MidpointRounding.AwayFromZero);
+
+					ret.value = color;
+					ret.isEdited = true;
+				}
 			}
 
 			return ret;
