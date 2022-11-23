41288f46146aa169e66cd237ec8c4a02c8cc443e
durswd <swda.durl@gmail.com>
2022-11-23 21:26:49 +0900

    Add a code to debug gl

--- Dev/Cpp/Test/Runtime/EffectPlatformGLFW.cpp
+++ Dev/Cpp/Test/Runtime/EffectPlatformGLFW.cpp
@@ -1,5 +1,20 @@
+#if defined(_WIN32)
+#define GLEW_STATIC
+typedef char GLchar;
+#include <GL/glew.h>
+#endif
 
 #include "EffectPlatformGLFW.h"
+
+#include <iostream>
+
+void GLAPIENTRY glDebugOutput(GLenum source, GLenum type, GLuint id, GLenum severity, GLsizei length, const GLchar* message, const void* userParam)
+{
+	if (type != GL_DEBUG_TYPE_PORTABILITY && type != GL_DEBUG_TYPE_OTHER)
+	{
+		std::cout << message << std::endl;
+	}
+}
 
 void* EffectPlatformGLFW::GetNativePtr(int32_t index)
 {
@@ -72,6 +87,7 @@ void EffectPlatformGLFW::InitializeWindow()
 
 	if (isOpenGLMode_)
 	{
+		glfwWindowHint(GLFW_OPENGL_DEBUG_CONTEXT, GL_TRUE);
 #if !_WIN32
 		glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
 		glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 2);
@@ -95,5 +111,16 @@ void EffectPlatformGLFW::InitializeWindow()
 	if (isOpenGLMode_)
 	{
 		glfwMakeContextCurrent(glfwWindow_);
+		glewInit();
+
+		GLint flags;
+		glGetIntegerv(GL_CONTEXT_FLAGS, &flags);
+		if (flags & GL_CONTEXT_FLAG_DEBUG_BIT)
+		{
+			glEnable(GL_DEBUG_OUTPUT);
+			glEnable(GL_DEBUG_OUTPUT_SYNCHRONOUS);
+			glDebugMessageCallback(glDebugOutput, NULL);
+			glDebugMessageControl(GL_DONT_CARE, GL_DONT_CARE, GL_DONT_CARE, 0, NULL, GL_TRUE);
+		}
 	}
 }
