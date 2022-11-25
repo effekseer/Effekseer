ad5ab6b8ea21dccd1d262c65aa4b17d0191e7276
durswd <swda.durl@gmail.com>
2022-11-26 01:42:43 +0900

    Add definitions of flatbuffer

--- Dev/gen_flat_buffers.bat
+++ Dev/gen_flat_buffers.bat
@@ -1,5 +1,5 @@
 Cpp\3rdParty\flatbuffers\bin\flatc.exe --cpp -o Cpp/Effekseer/Effekseer/FB/ FlatBuffers/CommonStructures.fbs
 Cpp\3rdParty\flatbuffers\bin\flatc.exe --cpp -o Cpp/Effekseer/Effekseer/FB/ FlatBuffers/BasicSettings.fbs
 
-Cpp\3rdParty\flatbuffers\bin\flatc.exe --csharp -o Editor/EffekseerCore/FB/ FlatBuffers/CommonStructures.fbs
-Cpp\3rdParty\flatbuffers\bin\flatc.exe --csharp -o Editor/EffekseerCore/FB/ FlatBuffers/BasicSettings.fbs
+Cpp\3rdParty\flatbuffers\bin\flatc.exe --csharp --gen-onefile -o Editor/EffekseerCore/FB/ FlatBuffers/CommonStructures.fbs
+Cpp\3rdParty\flatbuffers\bin\flatc.exe --csharp --gen-onefile -o Editor/EffekseerCore/FB/ FlatBuffers/BasicSettings.fbs
