260ae5d5d8b188f35b2b7131779b3f12b0479c55
durswd <swda.durl@gmail.com>
2023-07-26 19:30:25 +0900

    Remove redundant codes

--- Examples/Multithread/main.cpp
+++ Examples/Multithread/main.cpp
@@ -256,23 +256,8 @@ void MainLoop()
 	}
 }
 
-#if _WIN32
-#include <windows.h>
-std::wstring ToWide(const char* pText);
-void GetDirectoryName(char* dst, char* src);
-#endif
-
-//----------------------------------------------------------------------------------
-//
-//----------------------------------------------------------------------------------
 int main(int argc, char** argv)
 {
-#if _WIN32
-	char current_path[MAX_PATH + 1];
-	GetDirectoryName(current_path, argv[0]);
-	SetCurrentDirectoryA(current_path);
-#endif
-
 	InitWindow();
 
 	// 描画用インスタンスの生成
@@ -352,45 +337,3 @@ int main(int argc, char** argv)
 
 	return 0;
 }
-//----------------------------------------------------------------------------------
-//
-//----------------------------------------------------------------------------------
-
-#if _WIN32
-static std::wstring ToWide(const char* pText)
-{
-	int Len = ::MultiByteToWideChar(CP_ACP, 0, pText, -1, NULL, 0);
-
-	wchar_t* pOut = new wchar_t[Len + 1];
-	::MultiByteToWideChar(CP_ACP, 0, pText, -1, pOut, Len);
-	std::wstring Out(pOut);
-	delete[] pOut;
-
-	return Out;
-}
-
-void GetDirectoryName(char* dst, char* src)
-{
-	auto Src = std::string(src);
-	int pos = 0;
-	int last = 0;
-	while (Src.c_str()[pos] != 0)
-	{
-		dst[pos] = Src.c_str()[pos];
-
-		if (Src.c_str()[pos] == L'\\' || Src.c_str()[pos] == L'/')
-		{
-			last = pos;
-		}
-
-		pos++;
-	}
-
-	dst[pos] = 0;
-	dst[last] = 0;
-}
-#endif
-
-//----------------------------------------------------------------------------------
-//
-//----------------------------------------------------------------------------------
