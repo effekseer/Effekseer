7879a7ce617d24e2fec73358477a4f872ca479f2
durswd <swda.durl@gmail.com>
2023-10-24 20:06:12 +0900

    fix

--- Script/Test_Install/src/main.cpp
+++ Script/Test_Install/src/main.cpp
@@ -2,11 +2,17 @@
 
 #ifdef _WIN32
 #include <EffekseerRendererDX11.h>
+#include <EffekseerSoundXAudio2.h>
 #endif
 
 int main()
 {
 	auto efkManager = ::Effekseer::Manager::Create(8000);
+
+#ifdef _WIN32
+	auto efkRenderer = ::EffekseerRendererDX11::CreateGraphicsDevice(nullptr, nullptr);
+	auto efkSound = ::EffekseerSound::Sound::Create(nullptr, 0, 0);
+#endif
 
 	return 0;
 }
