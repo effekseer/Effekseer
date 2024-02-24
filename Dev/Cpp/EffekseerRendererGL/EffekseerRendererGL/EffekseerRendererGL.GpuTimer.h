b025e7bc500167c8d191b23463240916d9e0587b
ueshita <nalto32@gmail.com>
2024-02-24 15:03:36 +0900

    Add stage feature of GpuTimer

--- Dev/Cpp/EffekseerRendererGL/EffekseerRendererGL/EffekseerRendererGL.GpuTimer.h
+++ Dev/Cpp/EffekseerRendererGL/EffekseerRendererGL/EffekseerRendererGL.GpuTimer.h
@@ -27,19 +27,19 @@ public:
 
 	void InitDevice();
 	void ReleaseDevice();
-	void UpdateResults();
+	void UpdateResults(Effekseer::GpuStage stage);
 
 public: // For device restore
 	virtual void OnLostDevice();
 	virtual void OnResetDevice();
 
 public: // GpuTimer
-	virtual void BeginFrame() override;
-	virtual void EndFrame() override;
+	virtual void BeginStage(Effekseer::GpuStage stage) override;
+	virtual void EndStage(Effekseer::GpuStage stage) override;
 	virtual void AddTimer(const void* object) override;
 	virtual void RemoveTimer(const void* object) override;
-	virtual void Start(const void* object, uint32_t phase) override;
-	virtual void Stop(const void* object, uint32_t phase) override;
+	virtual void Start(const void* object) override;
+	virtual void Stop(const void* object) override;
 	virtual int32_t GetResult(const void* object) override;
 
 private:
@@ -47,19 +47,20 @@ private:
 
 	struct TimeData
 	{
-		GLuint timeElapsedQuery[NUM_PHASES];
-		bool queryRequested[NUM_PHASES] = {};
+		GLuint timeElapsedQuery[NUM_PHASES] = {};
+		Effekseer::GpuStage queryedStage[NUM_PHASES] = {};
 		int32_t result = 0;
 	};
 	std::unordered_map<const void*, TimeData> m_timeData;
 	
-	enum class State {
+	enum class State : uint8_t {
 		NoResult,
-		DuringFrame,
-		AfterFrame,
+		DuringStage,
+		AfterStage,
 		ResultUpdated,
 	};
-	State m_state = State::NoResult;
+	State m_stageState[8] = {};
+	Effekseer::GpuStage m_currentStage = {};
 };
 
 //-----------------------------------------------------------------------------------
