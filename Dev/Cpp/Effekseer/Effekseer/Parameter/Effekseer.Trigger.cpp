9b16fd8e1f380e644999c114453d3f6f386201ef
durswd <swda.durl@gmail.com>
2025-11-11 22:10:27 +0900

    Refactor Trigger codes

---
+++ Dev/Cpp/Effekseer/Effekseer/Parameter/Effekseer.Trigger.cpp
@@ -0,0 +1,47 @@
+﻿
+#include "Effekseer.Trigger.h"
+
+#include <cstring>
+
+#include "../Utils/Effekseer.BinaryVersion.h"
+
+namespace Effekseer
+{
+
+void TriggerParameter::Load(uint8_t*& pos, int32_t version)
+{
+	ToStartGeneration = {};
+	ToStopGeneration = {};
+	ToRemove = {};
+
+	if (version < Version17Alpha1)
+	{
+		return;
+	}
+
+	uint8_t flags = 0;
+	memcpy(&flags, pos, sizeof(uint8_t));
+	pos += sizeof(uint8_t);
+
+	auto loadValue = [&pos](TriggerValues& dst) {
+		memcpy(&dst, pos, sizeof(TriggerValues));
+		pos += sizeof(TriggerValues);
+	};
+
+	if (flags & (1 << 0))
+	{
+		loadValue(ToStartGeneration);
+	}
+
+	if (flags & (1 << 1))
+	{
+		loadValue(ToStopGeneration);
+	}
+
+	if (flags & (1 << 2))
+	{
+		loadValue(ToRemove);
+	}
+}
+
+} // namespace Effekseer
---
+++ Dev/Cpp/Effekseer/Effekseer/Parameter/Effekseer.Trigger.h
@@ -0,0 +1,33 @@
+﻿
+#ifndef __EFFEKSEER_PARAMETER_TRIGGER_H__
+#define __EFFEKSEER_PARAMETER_TRIGGER_H__
+
+#include "../Effekseer.Base.h"
+
+namespace Effekseer
+{
+
+enum class TriggerType : uint8_t
+{
+	None = 0,
+	ExternalTrigger = 1,
+};
+
+struct alignas(2) TriggerValues
+{
+	TriggerType type = TriggerType::None;
+	uint8_t index = 0;
+};
+
+struct TriggerParameter
+{
+	TriggerValues ToStartGeneration;
+	TriggerValues ToStopGeneration;
+	TriggerValues ToRemove;
+
+	void Load(uint8_t*& pos, int32_t version);
+};
+
+} // namespace Effekseer
+
+#endif // __EFFEKSEER_PARAMETER_TRIGGER_H__
