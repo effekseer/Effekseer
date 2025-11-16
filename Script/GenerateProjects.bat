e9bef7b7f9dfc7b5d2b1df43e69fe60b4926d0e8
durswd <swda.durl@gmail.com>
2025-11-16 14:06:39 +0900

    Fix to build cmake

--- Script/GenerateProjects.bat
+++ Script/GenerateProjects.bat
@@ -5,6 +5,6 @@ mkdir ..\build
 
 cd /d ..\build
 
-cmake -A x64 -D BUILD_VIEWER=ON -D BUILD_EDITOR=ON -D BUILD_TEST=ON ../
+cmake -A x64 -D BUILD_VIEWER=ON -D BUILD_EDITOR=ON -D BUILD_TEST=ON -DCMAKE_POLICY_VERSION_MINIMUM=3.5 ../
 
 pause
